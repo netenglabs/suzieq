4ce96eb09fb2c4800c6018a3fe59c735b2b90141
Dinesh Dutt <dd.ps4u@gmail.com>
2018-11-18 22:20:30 -0800

    New ipython initfile after code refactoring

--- ipython-initfile.py
+++ ipython-initfile.py
@@ -1,66 +1,29 @@
 import sys
+import os
 import qgrid
-import pandas as pd
 import json
 import warnings
-import os
+import re
+from pathlib import Path
+from collections import defaultdict
+import tempfile
+
+import pandas as pd
 import pyarrow as pa
 import pyarrow.parquet as pq
-from pathlib import Path
+import yaml
+
+from IPython.display import display, HTML
 
 sys.path.append("/home/ddutt/work/suzieq/")
 
+from utils import load_sq_config, get_schemas, get_query_output
 from livylib import get_livysession, exec_livycode
 
 is_notebook = False
 qgrid_enable = True
-
-
-def get_latest_df(folder):
-    lsd = []
-
-    for root, dirs, files in os.walk(folder):
-        if dirs and dirs[0].startswith('timestamp'):
-            dirs.sort()
-            lsd.append(os.path.join(root, dirs[-1]))
-
-    pdf_list = []
-    for file in lsd:
-        df = pd.read_parquet(file)
-        p = Path(file).parts
-        for elem in p:
-            if '=' in elem:
-                k, v = elem.split('=')
-                df[k] = v
-        pdf_list.append(df)
-
-    if pdf_list:
-        final_df = pd.concat(pdf_list)
-    else:
-        final_df = None
-
-    return final_df
-
-
-def get_df_for_window(folder, start=None, end=None):
-
-    if not start and not end:
-        return get_latest_df(folder)
-    elif start and not end:
-        timeset = pd.date_range(pd.to_datetime(start), periods=2, freq='15min')
-    elif end and not start:
-        timeset = pd.date_range(end=pd.to_datetime(end), periods=2, freq='15min')
-    else:
-        timeset = [pd.to_datetime(start), pd.to_datetime(end)]
-
-    table = pq.ParquetDataset(folder,
-                              filters=[('timestamp', '>', timeset[0]),
-                                       ('timestamp', '<', timeset[-1])]) \
-              .read()
-
-    df = table.to_pandas()
-
-    return df
+cfg = defaultdict(lambda: '')
+schemas = defaultdict(lambda: [])
 
 
 def query(qstr):
@@ -72,31 +35,38 @@ def query(qstr):
 
     # Replace any double quoted strings to single quotes first
     qstr.replace("'", '"')
+    qstr = qstr.strip()
+    dfolder = cfg['data-directory']
 
-    if is_notebook:
-        if qgrid_enable:
-            sstr = 'spark.sql("{0}").toJSON().collect()'.format(qstr)
+    if qstr == 'show tables':
+        if dfolder:
+            p = Path(dfolder)
+            tables = [{'table': dir.parts[-1]} for dir in p.iterdir()
+                      if dir.is_dir() and not dir.parts[-1].startswith('_')]
+            df = pd.DataFrame.from_dict(tables)
+    elif qstr.startswith('describe table'):
+        words = re.split(r'describe table\s+', qstr)
+        if len(words) > 1 and words[1].strip():
+            words = words[1].strip()
+            if re.search(r'\s+', words):
+                table = words.split()[0]
+            else:
+                table = words
+            entries = [{'name': x['name'], 'type': x['type']}
+                       for x in schemas[table]]
+            df = pd.DataFrame.from_dict(entries)
         else:
-            sstr = 'spark.sql("{0}").toPandas().to_html()'.format(qstr)
+            df = pd.DataFrame.from_dict({'error': 'Invalid command'})
     else:
-        sstr = 'spark.sql("{0}").show()'.format(qstr)
+        df = get_query_output(qstr, cfg, schemas)
 
-    output = exec_livycode(sstr, session_url)
     if is_notebook:
         if qgrid_enable:
-            jout = output['data']['text/plain']
-            return qgrid.show_grid(pd.read_json(
-                jout.replace("\', u\'", ', ').replace("u\'", '')
-                .replace("\'", ''), orient='frame'),
-                grid_options=grid_options)
-        return display(HTML(output['data']['text/plain']
-                            .replace('\\n', ' ')
-                            .replace("u\'", '')[:-1]))
+            return qgrid.show_grid(df, grid_options=grid_options)
+        else:
+            return display(df.style)
     else:
-        print(output['data']['text/plain']
-              .replace('\\n', ' ')
-              .replace("u\'", ''))
-    return
+        print(df)
 
 
 def lpm(saddr, host='.*'):
@@ -139,18 +109,25 @@ def get_vrflist(session_url, host='.*'):
         '''spark.sql("select distinct ifname from link where type == 'vrf' and host regexp '{0}'").toJSON().collect()'''.format(host), session_url, 'ifname')
 
 
-if 'ipykernel' in str(type(get_ipython())):
-    is_notebook = True
+if __name__ == '__main__':
+    if 'ipykernel' in str(type(get_ipython())):
+        is_notebook = True
 
-try:
-    session_url = get_livysession()
-except Exception:
-    session_url = None
-        
-if not session_url:
-    print('Unable to find valid, active Livy session')
-    print('Queries will not execute')
+    cfg = load_sq_config()
 
-# Need this for jupyter notebook
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+    schemas = get_schemas(cfg['schema-directory'])
+
+    try:
+        session_url = get_livysession()
+    except Exception:
+        session_url = None
+
+    if not session_url:
+        print('Unable to find valid, active Livy session')
+        print('Queries will not execute')
+
+    # Need this for jupyter notebook
+    warnings.filterwarnings("ignore", message="numpy.dtype size changed")
+    warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
+
+
