3e9306e6cf65ba7b06c54d28a66de57c8edbcb24
Dinesh Dutt <dd.ps4u@gmail.com>
2018-11-22 11:21:31 -0800

    initial commit of analysis code

--- cli/commands/analyze_commands.py
+++ cli/commands/analyze_commands.py
@@ -21,14 +21,14 @@ from nubia import command, argument
 import typing
 
 sys.path.append('/home/ddutt/work/')
-from suzieq.livylib import get_livysession, exec_livycode
 from suzieq.utils import load_sq_config, get_schemas
-from suzieq.utils import get_table_df
+from suzieq.utils import get_query_output
 
 
 @command('analyze', help="Analyze this")
 class AnalyzeCommand:
 
+    @argument("datacenter", description="datacenter to qualify selection")
     @argument("hostname", description="Name of host to qualify selection")
     @argument("start_time",
               description="Start of time window in YYYY-MM-dd HH:mm:SS format")
@@ -37,9 +37,11 @@ class AnalyzeCommand:
     @argument("view", description="view all records or just the latest",
               choices=["all", "latest"])
     def __init__(self, hostname: typing.List[str] = [], start_time: str = '',
-                 end_time: str = '', view: str = 'latest') -> None:
+                 end_time: str = '', view: str = 'latest',
+                 datacenter: typing.List[str] = []) -> None:
         self._cfg = load_sq_config(validate=False)
         self._schemas = get_schemas(self._cfg['schema-directory'])
+        self.datacenter = datacenter
         self.hostname = hostname
         self.start_time = start_time
         self.end_time = end_time
@@ -52,40 +54,68 @@ class AnalyzeCommand:
     @property
     def schemas(self):
         return self._schemas
-    """show various pieces of information"""
+    '''Go deeper into the data'''
 
-    @command('bgp')
-    @argument("peer", description="Name of peer to qualify show")
-    @argument("vrf", description="VRF to qualify show")
-    @argument("state", description="BGP neighbor state to qualify",
-              choices=["Established", "NotEstd"])
-    def analyze_bgp(self, peer: typing.List[str] = None,
-                    vrf: typing.List[str] = None, state: str = ''):
-        """
-        Show BGP
-        """
-        order_by = 'order by hostname, vrf, peer'
-        df = get_table_df('bgp', self.start_time, self.end_time,
-                          self.view, order_by, self.cfg, self.schemas,
-                          hostname=self.hostname, vrf=vrf, peer=peer,
-                          state=state)
-        print(df)
+    @command('uplink-ratio')
+    @argument("ifname", description="interface name to qualify show")
+    @argument("dir", description="Tx or Rx", choices=['rx', 'tx'])
+    def uplink_ratio(self, dir: str, ifname: typing.List[str] = None):
+        '''
+        Get interface bandwidth
+        '''
+        col_name = 'txBytes' if dir == 'tx' else 'rxBytes'
 
-    @command('interface')
-    @argument("peer", description="Name of peer to qualify show")
-    @argument("vrf", description="VRF to qualify show")
-    @argument("state", description="BGP neighbor state to qualify",
-              choices=["Established", "NotEstd"])
-    def analyze_interface(self, peer: typing.List[str] = None,
-                          vrf: typing.List[str] = None, state: str = ''):
-        """
-        Show BGP
-        """
-        order_by = 'order by hostname, vrf, peer'
-        df = get_table_df('bgp', self.start_time, self.end_time,
-                          self.view, order_by, self.cfg, self.schemas,
-                          hostname=self.hostname, vrf=vrf, peer=peer,
-                          state=state)
-        print(df)
+        ifname_str = '('
+        if isinstance(ifname, str):
+            ifname = [ifname]
 
+        if isinstance(self.hostname, str):
+            self.hostname = [self.hostname]
 
+        for i, ele in enumerate(ifname):
+            prefix = ' or ' if i else ''
+            ifname_str += "{}ifname=='{}'".format(prefix, ele)
+
+        ifname_str += ')'
+
+        hostname_str = '('
+        for i, ele in enumerate(self.hostname):
+            prefix = ' or ' if i else ''
+            hostname_str += "{}hostname=='{}'".format(prefix, ele)
+        hostname_str += ')'
+
+        qstr = ("select hostname, ifname, {}, timestamp from ifCounters "
+                "where {} and {} order by hostname, ifname, timestamp"
+                .format(col_name, hostname_str, ifname_str))
+
+        df = get_query_output(qstr, self.cfg, self.schemas, self.start_time,
+                              self.end_time, view='all')
+        df['prevBytes'] = df.groupby(['hostname', 'ifname'])[col_name].shift(1)
+        df['prevTime'] = df.groupby(['hostname', 'ifname'])['timestamp'].shift(1)
+
+        for hele in self.hostname:
+            dflist = []
+            for iele in ifname:
+                subdf = df.where((df['hostname'] == hele) &
+                                 (df['ifname'] == iele))
+                subdf = subdf.dropna()
+                subdf[iele] = ((subdf[col_name].sub(subdf['prevBytes']) * 8)
+                               / (subdf['timestamp'].sub(subdf['prevTime'])))
+                subdf['timestamp'] = pd.to_datetime(subdf['timestamp'],
+                                                    unit='ms')
+                dflist.append(subdf.drop(columns=[col_name, 'ifname',
+                                                  'prevBytes', 'prevTime']))
+
+            for i, subdf in enumerate(dflist[1:]):
+                newdf = pd.merge(dflist[0],
+                                 dflist[1][['timestamp', ifname[i+1]]],
+                                 on='timestamp', how='left')
+            
+            for iele in ifname[1:]:
+                newdf['%s:%s'%(iele, ifname[0])] = newdf[iele]/newdf[ifname[0]]
+
+            newdf = newdf.drop(columns=ifname)
+
+            print(newdf.describe())
+
+            
