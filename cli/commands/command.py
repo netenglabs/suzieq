e02fb39f71ced586fc1506e6c0dd4b9a8eee6f08
Dinesh Dutt <dd.ps4u@gmail.com>
2019-04-04 09:23:40 -0700

    Make system_df retrieval a property, add parent method to return only valid DF

--- cli/commands/command.py
+++ cli/commands/command.py
@@ -12,6 +12,7 @@ from nubia import command, argument, context
 import typing
 
 sys.path.append('/home/ddutt/work/')
+from suzieq.utils import get_table_df, get_query_df
 
 
 class SQCommand:
@@ -70,3 +71,55 @@ class SQCommand:
     def schemas(self):
         return self._schemas
     """show various pieces of information"""
+
+    @property
+    def system_df(self):
+        if self.ctxt.system_df is None:
+            sys_cols = ['datacenter', 'hostname', 'timestamp']
+            sys_sort = ['datacenter', 'hostname']
+
+            system_df = get_table_df('system', self.start_time, self.end_time,
+                                     self.view, sys_sort, self.cfg,
+                                     self.schemas, self.engine,
+                                     datacenter=self.datacenter,
+                                     columns=sys_cols)
+            self.ctxt.system_df = system_df
+
+        return self.ctxt.system_df
+
+    def get_valid_df(self, table, sort_fields, **kwargs):
+        table_df = get_table_df(table, self.start_time, self.end_time,
+                                self.view, sort_fields, self.cfg,
+                                self.schemas, self.engine, **kwargs)
+
+        if table_df.empty:
+            return table_df
+
+        if table != 'system':
+            # This merge is required to ensure that we don't serve out
+            # stale data that was obtained before the current run of
+            # the agent or from before the system came up
+            # We need the system DF cached to avoid slowdown in serving
+            # data.
+            # TODO: Find a way to invalidate the system df cache.
+
+            drop_cols = ['timestamp_y']
+
+            if self.system_df.empty:
+                return self.system_df
+
+            final_df = table_df.merge(self.system_df,
+                                      on=['datacenter', 'hostname']) \
+                               .dropna(how='any') \
+                               .query('timestamp_x >= timestamp_y') \
+                               .drop(columns=drop_cols) \
+                               .rename(index=str, columns={
+                                   'datacenter_x': 'datacenter',
+                                   'hostname_x': 'hostname',
+                                   'timestamp_x': 'timestamp'})
+        else:
+            final_df = table_df
+
+        return(final_df)
+
+
