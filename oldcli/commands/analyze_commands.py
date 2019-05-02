58004489fb8b7e672a8cffcb214f98c053bd3794
Dinesh Dutt <dd.ps4u@gmail.com>
2019-05-01 17:39:22 -0700

    Moving the older CLI out of main code

---
+++ oldcli/commands/analyze_commands.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Dinesh G Dutt
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import re
+import sys
+from pathlib import Path
+import json
+from collections import OrderedDict
+
+
+import pandas as pd
+from termcolor import cprint
+from nubia import command, argument
+import typing
+
+sys.path.append('/home/ddutt/work/')
+from suzieq.utils import get_query_df
+from suzieq.cli.commands.command import SQCommand
+
+
+@command('analyze', help="Analyze this")
+class AnalyzeCommand(SQCommand):
+    '''Analyze commands'''
+    @command('uplink-ratio')
+    @argument("ifname", description="interface name to qualify show")
+    @argument("dir", description="Tx or Rx", choices=['rx', 'tx'])
+    def uplink_ratio(self, dir: str, ifname: typing.List[str] = None):
+        '''
+        Get interface bandwidth
+        '''
+        col_name = 'txBytes' if dir == 'tx' else 'rxBytes'
+
+        ifname_str = '('
+        if isinstance(ifname, str):
+            ifname = [ifname]
+
+        if isinstance(self.hostname, str):
+            self.hostname = [self.hostname]
+
+        for i, ele in enumerate(ifname):
+            prefix = ' or ' if i else ''
+            ifname_str += "{}ifname=='{}'".format(prefix, ele)
+
+        ifname_str += ')'
+
+        hostname_str = '('
+        for i, ele in enumerate(self.hostname):
+            prefix = ' or ' if i else ''
+            hostname_str += "{}hostname=='{}'".format(prefix, ele)
+        hostname_str += ')'
+
+        qstr = ("select hostname, ifname, {}, timestamp from ifCounters "
+                "where {} and {} order by hostname, ifname, timestamp"
+                .format(col_name, hostname_str, ifname_str))
+
+        df = get_query_df(qstr, self.cfg, self.schemas, self.start_time,
+                          self.end_time, view='all')
+        if df is None or 'error' in df:
+            print('ERROR: {}'.format(df['type']))
+            return
+
+        df['prevBytes'] = df.groupby(['hostname', 'ifname'])[col_name] \
+                            .shift(1)
+        df['prevTime'] = df.groupby(['hostname', 'ifname'])['timestamp'] \
+                           .shift(1)
+
+        for hele in self.hostname:
+            dflist = []
+            for iele in ifname:
+                subdf = df.where((df['hostname'] == hele) &
+                                 (df['ifname'] == iele))
+                subdf = subdf.dropna()
+                subdf[iele] = ((subdf[col_name].sub(subdf['prevBytes']) * 8)
+                               / (subdf['timestamp'].sub(subdf['prevTime'])))
+                subdf['timestamp'] = pd.to_datetime(subdf['timestamp'],
+                                                    unit='ms')
+                dflist.append(subdf.drop(columns=[col_name, 'ifname',
+                                                  'prevBytes', 'prevTime']))
+
+            for i, subdf in enumerate(dflist[1:]):
+                newdf = pd.merge(dflist[0],
+                                 dflist[1][['timestamp', ifname[i+1]]],
+                                 on='timestamp', how='left')
+
+            for iele in ifname[1:]:
+                newdf['%s:%s' % (iele, ifname[0])] = (newdf[iele] /
+                                                      newdf[ifname[0]])
+
+            newdf = newdf.drop(columns=ifname)
+
+            print(newdf.describe())
+
+
---
+++ oldcli/commands/show_commands.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Dinesh G Dutt
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+
+import re
+import sys
+from pathlib import Path
+import json
+from collections import OrderedDict
+import time
+
+import pandas as pd
+from termcolor import cprint
+from nubia import command, argument, context
+import typing
+
+sys.path.append('/home/ddutt/work/')
+from suzieq.utils import get_ifbw_df
+from suzieq.cli.commands.command import SQCommand
+
+
+@command('show', help="show various pieces of information")
+class ShowCommand(SQCommand):
+    '''Show Commands'''
+
+    def _show_run(self, table, sort_fields, **kwargs):
+        '''Workshorse show command'''
+
+        now = time.time()
+        kwargs['columns'] = self.columns
+        if self.columns != ['default']:
+            sort_fields = None
+        table_df = self.get_valid_df(table, sort_fields, **kwargs)
+
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+
+        return(table_df)
+
+    @command('system')
+    @argument("vendor", description="vendor to qualify the output")
+    @argument("os", description="OS to qualify the output")
+    def show_system(self, vendor: typing.List[str] = None,
+                    os: typing.List[str] = None) -> None:
+        """
+        Show System Info such as model, OS
+        """
+        sort_fields = ['datacenter', 'hostname', 'vendor']
+        df = self._show_run('system', sort_fields,
+                            hostname=self.hostname, vendor=vendor, os=os,
+                            datacenter=self.datacenter)
+        if not df.empty:
+            df['bootupTimestamp'] = (pd.to_datetime(df['timestamp']) -
+                                     pd.to_datetime(df['bootupTimestamp'],
+                                                    unit='s'))
+        print(df)
+
+    @command('bgp')
+    @argument("peer", description="Name of peer to qualify show")
+    @argument("vrf", description="VRF to qualify show")
+    @argument("state", description="BGP neighbor state to qualify",
+              choices=["Established", "NotEstd"])
+    def show_bgp(self, peer: typing.List[str] = None,
+                 vrf: typing.List[str] = None, state: str = ''):
+        """
+        Show BGP
+        """
+        sort_fields = ['datacenter', 'hostname', 'vrf', 'peer']
+        df = self._show_run('bgp', sort_fields,
+                            hostname=self.hostname, vrf=vrf, peer=peer,
+                            state=state, datacenter=self.datacenter)
+        print(df)
+
+    @command('ospf')
+    @argument("ifname", description="Name of interface to qualify show")
+    @argument("vrf", description="VRF to qualify show")
+    @argument("state", description="BGP neighbor state to qualify",
+              choices=["full"])
+    @argument("type", description="Type of OSPF information to show",
+              choices=["neighbor", "interface"])
+    def show_ospf(self, ifname: typing.List[str] = None,
+                  vrf: typing.List[str] = None, state: str = '',
+                  type: str = 'neighbor'):
+        """
+        Show OSPF info
+        """
+        now = time.time()
+        sort_fields = ['datacenter', 'hostname', 'vrf', 'ifname']
+        if type == 'neighbor':
+            table = 'ospfNbr'
+        else:
+            table = 'ospfIf'
+
+        df = self._show_run(table, sort_fields,
+                            hostname=self.hostname, vrf=vrf,
+                            ifname=ifname, state=state,
+                            datacenter=self.datacenter)
+        print(df)
+
+    @command('interfaces')
+    @argument("ifname", description="interface name to qualify show")
+    @argument("state", description="interface state to qualify show",
+              choices=['up', 'down'])
+    def show_interfaces(self, ifname: typing.List[str] = [],
+                        state: str = ''):
+        """
+        Show interfaces
+        """
+        # Get the default display field names
+        sort_fields = ['datacenter', 'hostname', 'ifname']
+        if state:
+            df = self._show_run('interfaces', sort_fields,
+                                hostname=self.hostname, ifname=ifname,
+                                state=state, datacenter=self.datacenter)
+        else:
+            df = self._show_run('interfaces', sort_fields,
+                                hostname=self.hostname, ifname=ifname,
+                                datacenter=self.datacenter)
+
+        print(df)
+
+    @command('lldp')
+    @argument("ifname", description="interface name to qualify show")
+    def show_lldp(self, ifname: typing.List[str] = None):
+        """
+        Show LLDP info
+        """
+        # Get the default display field names
+        sort_fields = ['datacenter', 'hostname', 'ifname']
+        df = self._show_run('lldp', sort_fields,
+                            hostname=self.hostname, ifname=ifname,
+                            datacenter=self.datacenter)
+        print(df)
+
+    @command('filesystem')
+    @argument("mountPoint", description="Mountpoint to filter by")
+    @argument("usedPercent", description="show only if used percentage is >=")
+    def show_fs(self, mountPoint: str = None, usedPercent: str = None):
+        """
+        Show filesystem info
+        """
+        # Get the default display field names
+        sort_fields = ['datacenter', 'hostname', 'mountPoint']
+        df = self._show_run('fs', sort_fields,
+                            hostname=self.hostname, mountPoint=mountPoint,
+                            usedPercent=usedPercent,
+                            datacenter=self.datacenter)
+        print(df)
+
+    @command('ifbw')
+    @argument("ifname", description="interface name to qualify show")
+    def show_ifbw(self, ifname: typing.List[str] = None):
+        """
+        Show interface bandwidth for given host/ifname
+        """
+        # Get the default display field names
+        columns = ['txBytes', 'txPackets', 'rxBytes', 'rxPackets']
+        now = time.time()
+        df = get_ifbw_df(self.datacenter, self.hostname, ifname, columns,
+                         self.start_time, self.end_time, self.cfg,
+                         self.schemas)
+
+        print(df)
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+
+    @command("tables")
+    def show_tables(self):
+        """
+        List all the tables we know of given the Suzieq config
+        """
+
+        dfolder = self.cfg['data-directory']
+
+        if dfolder:
+            p = Path(dfolder)
+            tables = [{'table': dir.parts[-1]} for dir in p.iterdir()
+                      if dir.is_dir() and not dir.parts[-1].startswith('_')]
+            df = pd.DataFrame.from_dict(tables)
+            cprint(df)
