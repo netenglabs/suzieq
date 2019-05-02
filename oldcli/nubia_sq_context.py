66bd2ed77921d0f1f5dd2fa30606f853cc1d62d9
Dinesh Dutt <dd.ps4u@gmail.com>
2019-05-01 17:39:22 -0700

    Moving the older CLI out of main code

---
+++ oldcli/nubia_sq_context.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import sys
+
+from nubia import context
+from nubia import exceptions
+from nubia import eventbus
+
+sys.path.append('/home/ddutt/work/')
+from suzieq.utils import load_sq_config, get_schemas
+
+
+class NubiaSuzieqContext(context.Context):
+
+    def __init__(self):
+        self.cfg = load_sq_config(validate=False)
+
+        self.schemas = get_schemas(self.cfg['schema-directory'])
+
+        self.datacenter = ''
+        self.hostname = ''
+        self.start_time = ''
+        self.end_time = ''
+        self.exec_time = ''
+        self.engine = 'spark'
+        self.system_df = None
+        super().__init__()
+
+    def on_connected(self, *args, **kwargs):
+        pass
+
+    def on_cli(self, cmd, args):
+        # dispatch the on connected message
+        self.verbose = args.verbose
+        self.registry.dispatch_message(eventbus.Message.CONNECTED)
+
+    def on_interactive(self, args):
+        self.verbose = args.verbose
+        ret = self._registry.find_command("connect").run_cli(args)
+        if ret:
+            raise exceptions.CommandError("Failed starting interactive mode")
+        # dispatch the on connected message
+        self.registry.dispatch_message(eventbus.Message.CONNECTED)
+
---
+++ oldcli/nubia_sq_statusbar.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+from pygments.token import Token
+
+from nubia import context
+from nubia import statusbar
+
+
+class NubiaSuzieqStatusBar(statusbar.StatusBar):
+    def __init__(self, ctx):
+        self._last_status = None
+        self.ctx = ctx
+
+    def get_rprompt_tokens(self, cli):
+        if self._last_status:
+            return [(Token.RPrompt, "Error: {}".format(self._last_status))]
+        return []
+
+    def set_last_command_status(self, status):
+        self._last_status = status
+
+    def get_tokens(self, cli):
+        spacer = (Token.Spacer, "  ")
+        if context.get_context().verbose:
+            is_verbose = (Token.Warn, "ON")
+        else:
+            is_verbose = (Token.Info, "OFF")
+        return [
+            (Token.Toolbar, "Suzieq"),
+            spacer,
+            (Token.Toolbar, "Verbose "),
+            spacer,
+            is_verbose,
+            spacer,
+            (Token.Toolbar, "Datacenter "),
+            spacer,
+            (Token.Info, ', '.join(self.ctx.datacenter)),
+            spacer,
+            (Token.Toolbar, "Hostname "),
+            spacer,
+            (Token.Info, ', '.join(self.ctx.hostname)),
+            spacer,
+            (Token.Toolbar, "StartTime "),
+            spacer,
+            (Token.Info, self.ctx.start_time),
+            spacer,
+            (Token.Toolbar, "EndTime "),
+            spacer,
+            (Token.Info, self.ctx.end_time),
+            spacer,
+            (Token.Toolbar, "Engine "),
+            spacer,
+            (Token.Info, self.ctx.engine),
+            spacer,
+            (Token.Toolbar, "Query Time "),
+            spacer,
+            (Token.Info, self.ctx.exec_time),
+        ]
