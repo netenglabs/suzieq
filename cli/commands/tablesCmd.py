3bf2d8651d8ec89a25d08afed320d9d0a05fae4b
Dinesh Dutt <dd.ps4u@gmail.com>
2019-05-02 22:06:35 -0700

    Added support for querying info about tables

---
+++ cli/commands/tablesCmd.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Dinesh G Dutt
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import sys
+import time
+
+from nubia import command, argument
+
+sys.path.append('/home/ddutt/work/')
+from suzieq.cli.commands.command import SQCommand
+from suzieq.cli.tables import tablesObj
+
+
+@command('tables', help="Information about the various tables")
+class tablesCmd(SQCommand):
+
+    def __init__(self, engine: str = '', hostname: str = '',
+                 start_time: str = '', end_time: str = '',
+                 view: str = 'latest', datacenter: str = '',
+                 columns: str = 'default') -> None:
+        self.tablesobj = tablesObj()
+        super().__init__(engine=engine, hostname=hostname,
+                         start_time=start_time, end_time=end_time,
+                         view=view, datacenter=datacenter, columns=columns)
+
+    @command('show')
+    def show(self, **kwargs):
+        """
+        Show Tables
+        """
+        now = time.time()
+        df = self.tablesobj.get(hostname=self.hostname,
+                                datacenter=self.datacenter)
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+        print(df)
+
+    @command('describe')
+    @argument("table", description="interface name to qualify")
+    def describe(self, table: str = '', **kwargs):
+        """
+        Describe fields in table
+        """
+        now = time.time()
+        df = self.tablesobj.describe(table=table)
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+        print(df)
+
