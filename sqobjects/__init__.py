904a8a15011dd3f6fce2a27fc86e31e401b38ba2
Dinesh Dutt <dd.ps4u@gmail.com>
2019-05-14 23:14:22 -0700

    Removed debug print

--- sqobjects/__init__.py
+++ sqobjects/__init__.py
@@ -14,5 +14,4 @@ name = "sqobjects"
 
 modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')]
-print(__all__)
 
