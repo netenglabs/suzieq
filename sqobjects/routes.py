257dde476e80c7cf9310b965482f8b256fcfd8f2
Dinesh Dutt <dd.ps4u@gmail.com>
2019-06-15 23:13:32 -0700

    oops! forgot to commit the basic objects

---
+++ sqobjects/routes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Dinesh G Dutt
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+import typing
+
+from suzieq.sqobjects import basicobj
+
+
+class routesObj(basicobj.SQObject):
+
+    def __init__(self, engine: str = '', hostname: typing.List[str] = [],
+                 start_time: str = '', end_time: str = '',
+                 view: str = 'latest', datacenter: typing.List[str] = [],
+                 columns: typing.List[str] = ['default'],
+                 context=None) -> None:
+        super().__init__(engine, hostname, start_time, end_time, view,
+                         datacenter, columns, context=context, table='routes')
+        self._sort_fields = ['hostname', 'vrf', 'prefix']
+        self._cat_fields = []
