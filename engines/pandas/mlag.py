f95b8d86b935f6621041a810a91bc2c24dc6ef09
Dinesh Dutt <dd.ps4u@gmail.com>
2019-12-27 21:59:42 -0800

    initial support for mlag object

---
+++ engines/pandas/mlag.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Dinesh G Dutt
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
+from .engineobj import SQEngineObject
+
+
+class mlagObj(SQEngineObject):
+    pass
