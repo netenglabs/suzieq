93ae87493d9e6e7319779fbcb4defad8b6e03198
Dinesh Dutt <dd.ps4u@gmail.com>
2020-01-15 21:14:39 -0800

    convert relative import statement to absolute

--- engines/pandas/system.py
+++ engines/pandas/system.py
@@ -7,8 +7,7 @@
 # LICENSE file in the root directory of this source tree.
 #
 
-from .engineobj import SQEngineObject
-
+from suzieq.engines.pandas.engineobj import SQEngineObject
 
 class systemObj(SQEngineObject):
     pass
