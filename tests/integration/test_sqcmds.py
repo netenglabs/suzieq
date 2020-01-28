4dd56547b6695a644384b039e4150e9c5bb8eb31
Justin Pietsch <jpietsch@gmail.com>
2020-01-28 14:16:17 -0800

    added ability to add arguments to tests, now test all commands for all svcs in sqcmds

--- tests/integration/test_sqcmds.py
+++ tests/integration/test_sqcmds.py
@@ -6,76 +6,70 @@ from suzieq.cli.sqcmds import *
 from suzieq.cli.sq_nubia_context import NubiaSuzieqContext
 
 # TODO
-# checkin
-#  figure out code review
-# add commands to services with more commands
-#  what do I do about commands that require parameters
+# set and clear context
 # test parameters filtering
-# go through aver/assert and figure out the different branches they are taking
-# get pycharm remote debugging and testing working
-# how does coverage work?
+
 # is it possible to split the commands as different tests, rather than just the services?
-# refactor sqcmds to not needs so much code -- this is a different task
 # only works if there is a suzieq-cfg.yml file, which it then over-rides
-# how do I make sure I check all svces and all commands
+# how do I make sure I check all svcs and all commands
 
-basic_cmds= ['show', 'summarize']
 
-# to add
-# columns length, column names?
-#  specific data?
-@pytest.mark.parametrize("svc, commands, size", [
-    ('addrCmd', basic_cmds, [324, 18]),
-    ('arpndCmd', basic_cmds, [592, 48]),
-    ('bgpCmd', basic_cmds, [352, 143]),
-    ('interfaceCmd', basic_cmds, [1518, 143]),
-    ('lldpCmd', basic_cmds, [352, 48]),
-    ('macsCmd', basic_cmds, [312, 48]),
-    ('mlagCmd', basic_cmds + ['describe'], [44, NotImplementedError, 143]),
-    ('routesCmd', basic_cmds, [2596, 66]),
-    ('systemCmd', basic_cmds, [140, 130]),
-    ('tablesCmd', basic_cmds, [14, LookupError]),  #don't know how to pass parameters, can't do describe
-    ('topcpuCmd', basic_cmds, [42, 18]),
-    ('topmemCmd', basic_cmds, [27, 18]),
-    ('vlanCmd', basic_cmds, [96, 78])
-])
-def test_commands(setup_nubia, svc, commands, size):
-    for cmd, sz in zip(commands, size):
-        if isinstance(sz, type) and isinstance(sz(), Exception):
-            with pytest.raises(sz):
-                execute_cmd(svc, cmd)
-        else:
-            print(type(sz))
-            s = execute_cmd(svc, cmd)
-            assert isinstance(s, DataFrame)
-            assert s.size == sz
+basic_cmds = ['show', 'summarize']
 
 
 # TODO
-# these need to move after we figure out a much better way of
-#  dealing with missing data
-@pytest.mark.parametrize("svc, commands", [
-    ('evpnVniCmd', basic_cmds),
-    ('ospfCmd', basic_cmds)
+# columns length, column names?
+#  specific data?
+@pytest.mark.parametrize("svc, commands, args, size,", [
+    ('addrCmd', basic_cmds, [None, None], [324, 18],),
+    ('arpndCmd', basic_cmds, [None, None], [592, 48]),
+    ('bgpCmd', basic_cmds, [None, None], [352, 143]),
+    ('evpnVniCmd', basic_cmds, [None, None], [FileNotFoundError, FileNotFoundError]),
+    # test data doesn't have evpn
+    ('interfaceCmd', basic_cmds + ['top', 'aver'], [None, None, None, None], [1518, 143, 60, 0]),
+    ('lldpCmd', basic_cmds, [None, None], [352, 48]),
+    ('macsCmd', basic_cmds, [None, None], [312, 48]),
+    ('mlagCmd', basic_cmds + ['describe'], [None, None, None], [44, NotImplementedError, 143]),
+    ('ospfCmd', basic_cmds + ['top', 'aver'], [None, None, None, None],
+     [FileNotFoundError, FileNotFoundError, FileNotFoundError, FileNotFoundError]),  # test data doesn't have OSPF
+    ('routesCmd', basic_cmds + ['lpm'], [None, None, '10.0.0.1'], [2596, 66, 143]),
+    ('systemCmd', basic_cmds, [None, None], [140, 130]),
+    ('tablesCmd', basic_cmds, [None, 'system'], [14, 22]),
+    ('topcpuCmd', basic_cmds, [None, None], [42, 18]),
+    ('topmemCmd', basic_cmds, [None, None], [27, 18]),
+    ('vlanCmd', basic_cmds, [None, None], [96, 78])
 ])
-def test_missing_data(setup_nubia, svc, commands):
-    for cmd in commands:
-        with pytest.raises(FileNotFoundError):
-            s = execute_cmd(svc, cmd)
+def test_commands(setup_nubia, svc, commands, args, size):
+    """ runs through all of the commands for each of the sqcmds
+    svc: a service
+    commands: for each service, the list of commands
+    args: arguments
+    size: for each command, expected size of returned data, or Exception of the command is invalid"""
+    for cmd, arg, sz in zip(commands, args, size):
+        if isinstance(sz, type) and isinstance(sz(), Exception):
+            with pytest.raises(sz):
+                execute_cmd(svc, cmd, arg)
+        else:
+            s = execute_cmd(svc, cmd, arg)
+            assert isinstance(s, DataFrame)
+            assert s.size == sz
 
 
 def test_summary_exception(setup_nubia):
     s = None
     with pytest.raises(AttributeError):
-        s = execute_cmd('systemCmd', 'foop')
+        s = execute_cmd('systemCmd', 'foop', None)
     assert s is None
 
 
-def execute_cmd(svc, cmd):
+def execute_cmd(svc, cmd, arg):
     # expect the svc class are in the module svc and also named svc
     module = globals()[svc]
     instance = getattr(module, svc)()
-    return getattr(instance, cmd)()
+    if arg is not None:
+        return getattr(instance, cmd)(arg)
+    else:
+        return getattr(instance, cmd)()
 
 
 @pytest.fixture
