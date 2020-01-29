52c589543c4071a5a12ba2e65d4138faec39eb91
Justin Pietsch <jpietsch@gmail.com>
2020-01-29 13:54:04 -0800

    added pytest.ini and moved figures to conftest.py; testing filtering and invalid filtering of sqcmds

---
+++ tests/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from suzieq.cli.sq_nubia_context import NubiaSuzieqContext
+
+
+@pytest.fixture(scope='module')
+def setup_nubia():
+    _setup_nubia()
+
+
+def _setup_nubia():
+    from suzieq.cli.sq_nubia_plugin import NubiaSuzieqPlugin
+    from nubia import Nubia
+    # monkey patching -- there might be a better way
+    plugin = NubiaSuzieqPlugin()
+    plugin.create_context = create_context
+
+    # this is just so that context can be created
+    shell = Nubia(name='test', plugin=plugin)
+
+
+def create_context():
+    config = create_context_config()
+    context = NubiaSuzieqContext()
+    context.cfg = config
+    return context
+
+
+def create_context_config():
+    config = {'schema-directory': './suzeiq/config',
+              'data-directory': './tests/data/basic_dual_bgp/parquet-out',
+              'temp-directory': '/tmp/suzieq',
+              'logging-level': 'WARNING'
+              }
+    return config
