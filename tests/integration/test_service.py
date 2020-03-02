3d578302ec0216dbba04099384f8669c8011409c
Justin Pietsch <jpietsch@gmail.com>
2020-03-02 12:53:39 -0800

    some pep8 fixes and cleanup

--- tests/integration/test_service.py
+++ tests/integration/test_service.py
@@ -1,10 +1,8 @@
-import json
 import os
 import pytest
 import yaml
-from unittest.mock import Mock, MagicMock
+from unittest.mock import MagicMock
 
-from suzieq.service import Service
 from suzieq.node import Node
 
 from functools import partial
@@ -17,6 +15,8 @@ from tests.conftest import tables
 #  test diff with data gathered twice (we have 2 sets of data, but aren't using the second set yet)
 #  make sure we test all child classes
 #  test writing to datastore? -- that seems big and lots of mocking
+#  test node returns that take a long time -- test timeouts
+
 
 @pytest.mark.service
 @pytest.mark.parametrize('service', tables)
@@ -28,7 +28,7 @@ def test_process_services_end_to_end(service, init_services_default, event_loop,
     assert len(nodes) > 0
     service.set_nodes(nodes)
 
-    #replicating some of service.run to deconstruct
+    # replicating some of service.run to deconstruct
     service.nodelist = list(nodes.keys())
     outputs = event_loop.run_until_complete(service.gather_data())
 
@@ -46,17 +46,14 @@ def test_process_services_end_to_end(service, init_services_default, event_loop,
     processed = _get_processed_data(service)
 
     # this is the cod necessary to write out the data
-    #file = tmp_path / f"{service.name}.yml"
-    #print(f"writing to {file}")
-    #file.write_text(yaml.dump(results))
+    # file = tmp_path / f"{service.name}.yml"
+    # print(f"writing to {file}")
+    # file.write_text(yaml.dump(results))
 
     assert yaml.dump(results) == yaml.dump(processed)
 
 
-
-
 def _get_processed_data(service):
-    data = {}
     file = os.path.abspath(os.curdir) + f"/tests/data/basic_dual_bgp/processed/{service.name}.yml"
 
     with open(file, 'r') as f:
@@ -94,4 +91,3 @@ async def _create_exec_serivce(devtype, cmd, data, svc_defn):
 
     assert defn_cmd == cmd
     return data
-
