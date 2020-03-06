401966ce893efb01894a104b2395b6d280263d9c
Dinesh Dutt <dd.ps4u@gmail.com>
2020-03-06 07:13:06 -0800

    Fix path of modules to search for services modules

---
+++ suzieq/poller/services/__init__.py
@@ -0,0 +1,141 @@
+from os.path import isfile, isdir, exists, dirname
+from os import walk
+import logging
+import yaml
+import json
+import textfsm
+from pkgutil import walk_packages
+from inspect import getmembers, isclass, getfile
+import importlib
+
+from suzieq.utils import avro_to_arrow_schema
+from .service import Service
+
+
+async def init_services(svc_dir, schema_dir, queue, run_once):
+    """Process service definitions by reading each file in svc dir"""
+
+    svcs_list = []
+
+    # Load up all the service definitions we can find
+    svc_classes = {}
+    for i in walk_packages(path=[dirname(getfile(Service))]):
+        for mbr in getmembers(importlib.import_module(
+                'suzieq.poller.services.'+i.name), isclass):
+            if mbr[0] == "Service" or not mbr[0].endswith("Service"):
+                continue
+            svc_classes[i.name] = mbr[1]
+            svc_classes[mbr[0]] = mbr[1]
+
+    if not isdir(svc_dir):
+        logging.error("services directory not a directory: {}".format(svc_dir))
+        return svcs_list
+
+    if not isdir(schema_dir):
+        logging.error("schema directory not a directory: {}".format(svc_dir))
+        return svcs_list
+
+    for root, _, filenames in walk(svc_dir):
+        for filename in filenames:
+            if filename.endswith(".yml") or filename.endswith(".yaml"):
+                with open(root + "/" + filename, "r") as f:
+                    svc_def = yaml.safe_load(f.read())
+                if "service" not in svc_def or "apply" not in svc_def:
+                    logging.error(
+                        'Ignoring invalid service file definition. \
+                    Need both "service" and "apply" keywords: {}'.format(
+                            filename
+                        )
+                    )
+                    continue
+
+                period = svc_def.get("period", 15)
+                for elem, val in svc_def["apply"].items():
+                    if "copy" in val:
+                        newval = svc_def["apply"].get(val["copy"], None)
+                        if not newval:
+                            logging.error(
+                                "No device type {} to copy from for "
+                                "{} for service {}".format(
+                                    val["copy"], elem, svc_def["service"]
+                                )
+                            )
+                            continue
+                        val = newval
+
+                    if "command" not in val or (
+                        "normalize" not in val and "textfsm" not in val
+                    ):
+                        logging.error(
+                            "Ignoring invalid service file "
+                            'definition. Need both "command" and '
+                            '"normalize/textfsm" keywords: {}, {}'.format(
+                                filename, val)
+                        )
+                        continue
+
+                    if "textfsm" in val:
+                        # We may have already visited this element and parsed
+                        # the textfsm file. Check for this
+                        if val["textfsm"] and isinstance(
+                            val["textfsm"], textfsm.TextFSM
+                        ):
+                            continue
+                        tfsm_file = svc_dir + "/" + val["textfsm"]
+                        if not isfile(tfsm_file):
+                            logging.error(
+                                "Textfsm file {} not found. Ignoring"
+                                " service".format(tfsm_file)
+                            )
+                            continue
+                        with open(tfsm_file, "r") as f:
+                            tfsm_template = textfsm.TextFSM(f)
+                            val["textfsm"] = tfsm_template
+                    else:
+                        tfsm_template = None
+
+                # Find matching schema file
+                fschema = "{}/{}.avsc".format(schema_dir, svc_def["service"])
+                if not exists(fschema):
+                    logging.error(
+                        "No schema file found for service {}. "
+                        "Ignoring service".format(svc_def["service"])
+                    )
+                    continue
+                else:
+                    with open(fschema, "r") as f:
+                        schema = json.loads(f.read())
+                    schema = avro_to_arrow_schema(schema)
+
+                # Valid service definition, add it to list
+                if svc_def["service"] in svc_classes:
+                    service = svc_classes[svc_def["service"]](
+                        svc_def["service"],
+                        svc_def["apply"],
+                        period,
+                        svc_def.get("type", "state"),
+                        svc_def.get("keys", []),
+                        svc_def.get("ignore-fields", []),
+                        schema,
+                        queue,
+                        run_once
+                    )
+                else:
+                    service = Service(
+                        svc_def["service"],
+                        svc_def["apply"],
+                        period,
+                        svc_def.get("type", "state"),
+                        svc_def.get("keys", []),
+                        svc_def.get("ignore-fields", []),
+                        schema,
+                        queue,
+                        run_once
+                    )
+
+                logging.info("Service {} added".format(service.name))
+                svcs_list.append(service)
+
+    return svcs_list
+
+__all__ = [Service, init_services]
