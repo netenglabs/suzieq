c26068a1404c5d63300b962a8777ebf7a4495888
Justin Pietsch <jpietsch@gmail.com>
2020-03-27 15:24:08 -0700

    updated BGP summarize and ordred its data

--- suzieq/engines/pandas/bgp.py
+++ suzieq/engines/pandas/bgp.py
@@ -14,50 +14,46 @@ class BgpObj(SqEngineObject):
 
         self._add_field_to_summary('hostname', 'nunique', 'hosts')
         self._add_field_to_summary('hostname', 'count', 'sessions')
-        for field in ['asn', 'vrf']:
+        for field in ['asn', 'vrf', 'peerAsn']:
             self._add_field_to_summary(field, 'nunique')
-
 
         ipv4_enabled = self.summary_df.query("v4Enabled")["namespace"].unique()
         ipv6_enabled = self.summary_df.query("v6Enabled")["namespace"].unique()
         evpn_enabled = self.summary_df.query("evpnEnabled")["namespace"].unique()
-        {self.ns[i].update({'afi-safi': []}) for i in self.ns.keys()}
-        {self.ns[i]['afi-safi'].append("ipv4")
-         for i in self.ns.keys() if i in ipv4_enabled}
-        {self.ns[i]['afi-safi'].append("ipv6")
-         for i in self.ns.keys() if i in ipv6_enabled}
-        {self.ns[i]['afi-safi'].append("evpn")
-         for i in self.ns.keys() if i in evpn_enabled}
 
         established = self.summary_df.query("state == 'Established'") \
             .groupby(by=['namespace'])
 
         uptime = established["estdTime"]
-        self._add_stats_to_summary(uptime, 'upTimes')
-
-        med_v4_updates = established["v4PfxRx"].median()
-        med_v6_updates = established["v6PfxRx"].median()
-        med_evpn_updates = established["evpnPfxRx"].median()
-        med_rx_updates = established["updatesRx"].median()
-        med_tx_updates = established["updatesTx"].median()
-
-
-
-        {self.ns[i].update({'medV4PfxRx': med_v4_updates[i]})
-         for i in med_v4_updates.keys()}
-        {self.ns[i].update({'medV6PfxRx': med_v6_updates[i]})
-         for i in med_v6_updates.keys()}
-        {self.ns[i].update({'medEvpnPfxRx': med_evpn_updates[i]})
-         for i in med_evpn_updates.keys()}
-        {self.ns[i].update({'medUpdatesRx': med_rx_updates[i]})
-         for i in med_rx_updates.keys()}
-        {self.ns[i].update({'medUpdatesTx': med_tx_updates[i]})
-         for i in med_tx_updates.keys()}
+        v4_updates = established["v4PfxRx"]
+        v6_updates = established["v6PfxRx"]
+        evpn_updates = established["evpnPfxRx"]
+        rx_updates = established["updatesRx"]
+        tx_updates = established["updatesTx"]
 
         down_sessions_per_ns = self.summary_df.query("state == 'NotEstd'")['namespace'] \
-                                 .value_counts()
-        {self.ns[i].update({'downSessions': 0}) for i in self.ns.keys()}
-        {self.ns[i].update({'downSessions': down_sessions_per_ns[i]})
-         for i in down_sessions_per_ns.keys()}
+            .value_counts()
 
-        return pd.DataFrame(self.ns).convert_dtypes()
+        self._add_stats_to_summary(uptime, 'upTimes')
+        self._add_stats_to_summary(v4_updates, 'V4PfxRx')
+        self._add_stats_to_summary(v6_updates, 'V6PfxRx')
+        self._add_stats_to_summary(evpn_updates, 'EvpnPfxRx')
+        self._add_stats_to_summary(rx_updates, 'UpdatesRx')
+        self._add_stats_to_summary(tx_updates, 'UpdatesTx')
+
+        for i in self.ns.keys():
+            self.ns[i].update({'afi-safi': []})
+            if i in ipv4_enabled:
+                self.ns[i]['afi-safi'].append("ipv4")
+            if i in ipv6_enabled:
+                self.ns[i]['afi-safi'].append("ipv6")
+            if i in evpn_enabled:
+                self.ns[i]['afi-safi'].append('evpn')
+            self.ns[i].update({'downSessions': down_sessions_per_ns.get(i, 0)})
+
+        self.summary_row_order = ['hosts', 'sessions', 'asn', 'vrf', 'afi-safi',
+                                  'downSessions', 'upTimes', 'V4PfxRx',
+                                  'V6PfxRx', 'EvpnPfxRx', 'UpdatesRx',
+                                  'UpdatesTx']
+        self._post_summarize()
+        return self.ns_df.convert_dtypes()
