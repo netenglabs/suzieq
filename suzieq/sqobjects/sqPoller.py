a70647144bb11a6e7cbfe6d8a4066a06fd548360
Justin Pietsch <jpietsch@gmail.com>
2020-03-30 16:05:34 -0700

    added rudimentary cmd line support for sqpoller and it gets the default summarize

---
+++ suzieq/sqobjects/sqPoller.py
@@ -0,0 +1,16 @@
+import typing
+
+from suzieq.sqobjects import basicobj
+
+
+class SqPollerObj(basicobj.SqObject):
+
+    def __init__(self, engine: str = '', hostname: typing.List[str] = [],
+                 start_time: str = '', end_time: str = '',
+                 view: str = 'latest', namespace: typing.List[str] = [],
+                 columns: typing.List[str] = ['default'],
+                 context=None) -> None:
+        super().__init__(engine, hostname, start_time, end_time, view,
+                         namespace, columns, context=context, table='sqPoller')
+        self._sort_fields = ['namespace', 'hostname']
+        self._cat_fields = []
