e21589133e2d6bd19ca02e124aa241e7a9d49bdc
Justin Pietsch <jpietsch@gmail.com>
2020-04-02 12:08:08 -0700

    added sqPoller summarize

--- suzieq/engines/pandas/mlag.py
+++ suzieq/engines/pandas/mlag.py
@@ -4,7 +4,6 @@ from .engineobj import SqEngineObject
 class MlagObj(SqEngineObject):
     pass
 
-
     def summarize(self, **kwargs):
         self._init_summarize(self.iobj._table, **kwargs)
         if self.summary_df.empty:
@@ -19,14 +18,12 @@ class MlagObj(SqEngineObject):
                       'domainId']:
             self._add_list_or_count_to_summary(field)
 
-
         # have to explode fields that are lists
         self.summary_df = self.summary_df.explode('mlagInterfacesList').dropna(how='any')
         self.nsgrp = self.summary_df.groupby(by=["namespace"])
 
         for field in ['mlagInterfacesList']:
             self._add_list_or_count_to_summary(field)
-
 
         self._post_summarize()
         return self.ns_df.convert_dtypes()
--- suzieq/engines/pandas/sqPoller.py
+++ suzieq/engines/pandas/sqPoller.py
@@ -3,3 +3,23 @@ from suzieq.engines.pandas.engineobj import SqEngineObject
 
 class SqpollerObj(SqEngineObject):
     pass
+
+    def summarize(self, **kwargs):
+        self._init_summarize(self.iobj._table, **kwargs)
+        if self.summary_df.empty:
+            return self.summary_df
+
+        self._add_field_to_summary('hostname', 'count', 'rows')
+        for field in ['hostname', 'service', 'emptyCount',
+                      'status']:
+            self._add_list_or_count_to_summary(field)
+        for field in ['pollExcdPeriodCount', 'gatherTime', 'nodeQsize',
+                      'svcQsize', 'totalTime', 'wrQsize']:
+            self._add_stats_to_summary(self.nsgrp[field], field)
+
+        self.summary_row_order = ['hostname', 'rows', 'service', 'status',
+                                  'pollExcdPeriodCount', 'nodeQsize',
+                                  'svcQsize', 'wrQsize', 'gatherTime',
+                                  'totalTime', 'emptyCount']
+        self._post_summarize()
+        return self.ns_df.convert_dtypes()
