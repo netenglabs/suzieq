2a7618de9dfe9584863f6670ebb013e159f91522
Dinesh Dutt <dd.ps4u@gmail.com>
2020-04-07 10:45:20 -0700

    Fix broken support for gathering data from Arista via REST API

--- suzieq/poller/services/bgp.py
+++ suzieq/poller/services/bgp.py
@@ -10,6 +10,15 @@ class BgpService(Service):
 
         # The AFI/SAFI key string changed in version 7.x of FRR and so we have
         # to munge the output to get the data out of the right key_fields
+        if raw_data.get("devtype", None) == "cumulus":
+            return self._clean_cumulus_data(processed_data, raw_data)
+        elif raw_data.get("devtype", None) == "eos":
+            return self._clean_eos_data(processed_data, raw_data)
+
+        return processed_data
+
+    def _clean_cumulus_data(self, processed_data, raw_data):
+
         for entry in processed_data:
             if entry["state"] == "Established":
                 if not (entry["v4Enabled"] or
@@ -26,4 +35,16 @@ class BgpService(Service):
                         entry["v4PfxRx"] = entry.pop("newv4PfxRx")
                         entry["v6PfxRx"] = entry.pop("newv6PfxRx")
                         entry["evpnPfxRx"] = entry.pop("newevpnPfxRx")
+        return processed_data
+
+    def _clean_eos_data(self, processed_data, raw_data):
+
+        for entry in processed_data:
+            if entry["bfdStatus"] == 3:
+                entry["bfdStatus"] = "up"
+            elif entry["bfdStatus"] != "disabled":
+                entry["bfdStatus"] = "down"
+            entry["asn"] = int(entry["asn"])
+            entry["peerAsn"] = int(entry["asn"])
+
         return processed_data
