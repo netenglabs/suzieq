ad0bb400d4ec1fef860823b351e7b9bcfd09b9dd
Justin Pietsch <jpietsch@gmail.com>
2020-04-08 16:29:41 -0700

    fixed sqPoller summarize for now

--- suzieq/engines/pandas/sqPoller.py
+++ suzieq/engines/pandas/sqPoller.py
@@ -10,16 +10,18 @@ class SqpollerObj(SqEngineObject):
             return self.summary_df
 
         self._add_field_to_summary('hostname', 'count', 'rows')
-        for field in ['hostname', 'service', 'emptyCount',
+        for field in ['hostname', 'service',
                       'status']:
             self._add_list_or_count_to_summary(field)
-        for field in ['pollExcdPeriodCount', 'gatherTime', 'nodeQsize',
-                      'svcQsize', 'totalTime', 'wrQsize']:
+        for field in ['pollExcdPeriodCount']:
+            # TODO: we need to do something with these.
+            #, 'gatherTime', 'nodeQsize',
+                      #'svcQsize', 'totalTime', 'wrQsize']:
             self._add_stats_to_summary(self.nsgrp[field], field)
 
         self.summary_row_order = ['hostname', 'rows', 'service', 'status',
-                                  'pollExcdPeriodCount', 'nodeQsize',
-                                  'svcQsize', 'wrQsize', 'gatherTime',
-                                  'totalTime', 'emptyCount']
+                                  'pollExcdPeriodCount']#, 'nodeQsize',
+                                  #'svcQsize', 'wrQsize', 'gatherTime',
+                                  #'totalTime']
         self._post_summarize()
         return self.ns_df.convert_dtypes()
