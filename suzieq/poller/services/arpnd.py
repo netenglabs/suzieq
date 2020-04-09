96d1db0862b1fd39c20eed83034e7535c61befa4
Dinesh Dutt <dd.ps4u@gmail.com>
2020-04-09 15:26:12 -0700

    Support for gathering ARP data from EOS devices

--- suzieq/poller/services/arpnd.py
+++ suzieq/poller/services/arpnd.py
@@ -7,9 +7,15 @@ class ArpndService(Service):
     def clean_data(self, processed_data, raw_data):
 
         devtype = raw_data.get("devtype", None)
-        if any([devtype == x for x in ["cumulus", "linux", "platina"]]):
+        if any([devtype == x for x in ["cumulus", "linux"]]):
             for entry in processed_data:
                 entry["offload"] = entry["offload"] == "offload"
                 entry["state"] = entry["state"].lower()
+                if entry["state"] == "stale" or entry["state"] == "delay":
+                    entry["state"] = "reachable"
+        elif devtype == 'eos':
+            for entry in processed_data:
+                entry['macaddr'] = ':'.join(
+                    [f'{x[:2]}:{x[2:]}' for x in entry['macaddr'].split('.')])
 
         return super().clean_data(processed_data, raw_data)
