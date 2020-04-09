edcdc560fcec17be8444393aaab242fd1817478e
Justin Pietsch <jpietsch@gmail.com>
2020-04-08 21:17:10 -0700

    updating the data for testing that shoul dbe correct this time. including the updated yml files

--- tests/utilities/generate_data.sh
+++ tests/utilities/generate_data.sh
@@ -7,19 +7,19 @@ suzieq_dir=/tmp/pycharm_project_304/suzieq/suzieq
 run_sqpoller () {
     topology="$1"
     name="$2"
-    echo $name
-    ansible_dir=~/cloud-native-data-center-networking/topologies/$topology/.vagrant
-    ansible_file=$ansible_dir/provisioners/ansible/inventory/vagrant_ansible_inventory
-    sudo chown -R jpiet $ansible_dir
+    echo ${name}
+    ansible_dir=~/cloud-native-data-center-networking/topologies/${topology}/.vagrant
+    ansible_file=${ansible_dir}/provisioners/ansible/inventory/vagrant_ansible_inventory
+    sudo chown -R jpiet ${ansible_dir}
     echo "SUZIEQ"
-    python3 $suzieq_dir/poller/sq-poller -i $ansible_file -n $name &
+    python3 ${suzieq_dir}/poller/sq-poller -i ${ansible_file} -n ${name} &
     sleep 60
     pkill -f sq-poller
     ps auxwww | grep poller
     echo "SUZIEQ poller done"
     sleep 15
-    python3 $suzieq_dir/cli/suzieq-cli table show
-    python3 $suzieq_dir/cli/suzieq-cli device unique --columns=namespace
+    python3 ${suzieq_dir}/cli/suzieq-cli table show
+    python3 ${suzieq_dir}/cli/suzieq-cli device unique --columns=namespace
     
 
 }
@@ -37,7 +37,7 @@ run_scenario () {
     sudo ansible-playbook ping.yml
     RESULT=$?
     echo "PING RESULTS $RESULT"
-    run_sqpoller $topology $name
+    run_sqpoller ${topology} ${name}
 
 }
 
@@ -47,10 +47,10 @@ run_protos () {
     scenario="$3"
     name="$4"
 
-    echo $proto
+    echo ${proto}
     vagrant_up
-    cd $proto
-    run_scenario $topology $proto $scenario $name
+    cd ${proto}
+    run_scenario ${topology} ${proto} ${scenario} ${name}
     vagrant_down
     cd ..
 }
@@ -67,19 +67,21 @@ vagrant_up () {
     time sudo vagrant up
     echo "VAGRANT UP"
 }
-
+date
 rm -rf ~/parquet-out
 topology='dual-attach'
-cd $topology
-run_protos $topology evpn ospf-ibgp ospf-ibgp
-run_protos $topology evpn centralized dual-evpn
+cd ${topology}
+run_protos ${topology} evpn ospf-ibgp ospf-ibgp
+run_protos ${topology} evpn centralized dual-evpn
+cd ..
 topology='single-attach'
-cd $topology
-run_protos $toplogy ospf numbered ospf-single
-
+cd ${topology}
+run_protos ${topology} ospf numbered ospf-single
+cd ..
 mv ~/parquet-out ~/parquet-out-multidc
 
 topology='dual-attach'
-cd $topology
-run_protos $topology bgp numbered dual-bgp
+cd ${topology}
+run_protos ${topology} bgp numbered dual-bgp
 mv ~/parquet-out ~/parquet-out-basic_dual_bgp
+date
