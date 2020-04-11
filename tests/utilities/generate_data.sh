ff07f4f9d8b30fcc3d502cdfc824bc185f701cf7
Justin Pietsch <jpietsch@gmail.com>
2020-04-11 13:59:08 -0700

    updated test data and changes samples

--- tests/utilities/generate_data.sh
+++ tests/utilities/generate_data.sh
@@ -12,7 +12,7 @@ run_sqpoller () {
     ansible_file=${ansible_dir}/provisioners/ansible/inventory/vagrant_ansible_inventory
     sudo chown -R jpiet ${ansible_dir}
     echo "SUZIEQ"
-    python3 ${suzieq_dir}/poller/sq-poller -i ${ansible_file} -n ${name} &
+    python3 ${suzieq_dir}/poller/sq-poller -i ${ansible_file} -n ${name}  >> ${verbose_log} 2>&1 &
     sleep 60
     pkill -f sq-poller
     ps auxwww | grep poller
@@ -20,7 +20,6 @@ run_sqpoller () {
     sleep 15
     python3 ${suzieq_dir}/cli/suzieq-cli table show
     python3 ${suzieq_dir}/cli/suzieq-cli device unique --columns=namespace
-    
 
 }
 
@@ -29,15 +28,15 @@ run_scenario () {
     proto="$2"
     scenario="$3"
     name="$4"
-    echo "SCENARIO $topology $proto $scenario"
+    echo "SCENARIO $topology $proto $scenario" >> ${log} 2>&1
     pwd
-    time sudo ansible-playbook -b -e "scenario=$scenario" deploy.yml
-    echo "DEPLOY RESULTS $?"
+    time sudo ansible-playbook -b -e "scenario=$scenario" deploy.yml >> ${verbose_log} 2>&1
+    echo "DEPLOY RESULTS $?" >> ${log} 2>&1
     sleep 15 #on fast machines, not everything is all the way up without sleep
-    sudo ansible-playbook ping.yml
+    sudo ansible-playbook ping.yml >> ${verbose_log} 2>&1
     RESULT=$?
-    echo "PING RESULTS $RESULT"
-    run_sqpoller ${topology} ${name}
+    echo "PING RESULTS $RESULT" >> ${log} 2>&1
+    run_sqpoller ${topology} ${name} >> ${log} 2>&1
 
 }
 
@@ -47,7 +46,7 @@ run_protos () {
     scenario="$3"
     name="$4"
 
-    echo ${proto}
+    echo ${proto} >> ${log} 2>&1
     vagrant_up
     cd ${proto}
     run_scenario ${topology} ${proto} ${scenario} ${name}
@@ -56,18 +55,26 @@ run_protos () {
 }
 
 vagrant_down () {
-    echo "VAGRANT DESTROYING"
-    sudo vagrant destroy -f
-    echo "VAGRANT DESTROY RESULTS $?"
+    sudo vagrant destroy -f >> ${verbose_log} 2>&1
+    echo "VAGRANT DESTROY RESULTS $?" >> ${log} 2>&1
 
 }
 
 vagrant_up () {
-    sudo vagrant status
-    time sudo vagrant up
-    echo "VAGRANT UP"
+    echo 'foo' >> ${verbose_log}
+    time sudo vagrant up >> ${verbose_log} 2>&1
+    echo "VAGRANT UP" >> ${log} 2>&1
+    sudo vagrant status >> ${log} 2>&1
+
 }
-date
+
+log=`pwd`/log
+verbose_log=${log}.verbose
+echo ${log}
+echo ${verbose_log}
+date > ${log}
+date > ${verbose_log}
+
 rm -rf ~/parquet-out
 topology='dual-attach'
 cd ${topology}
@@ -84,4 +91,4 @@ topology='dual-attach'
 cd ${topology}
 run_protos ${topology} bgp numbered dual-bgp
 mv ~/parquet-out ~/parquet-out-basic_dual_bgp
-date
+date > ${log}
