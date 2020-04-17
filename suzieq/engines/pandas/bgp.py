85904fca79e713f03af860ebdeed41c65cad216e
Dinesh Dutt <dd.ps4u@gmail.com>
2020-04-16 23:36:28 -0700

    Fix bgp assert column names and ASN type

--- suzieq/engines/pandas/bgp.py
+++ suzieq/engines/pandas/bgp.py
@@ -194,5 +194,15 @@ class BgpObj(SqEngineObject):
         return (df[['namespace', 'hostname_x', 'vrf_x', 'peer_x', 'asn_x',
                     'peerAsn_x', 'state_x', 'peerHostname_x', 'vrf_y', 'peer_y',
                     'asn_y', 'peerAsn_y', 'assert',
-                    'assertReason']].explode(column="assertReason")
+                    'assertReason']]
+                .rename(columns={'hostname_x': 'hostname', 'vrf_x':
+                                 'vrf', 'peer_x': 'peer', 'asn_x': 'asn',
+                                 'peerAsn_x': 'peerAsn', 'state_x': 'state',
+                                 'peerHostname_x': 'hostnamePeer',
+                                 'vrf_y': 'vrfPeer', 'peer_y': 'peerPeer',
+                                 'asn_y': 'asnPeer',
+                                 'peerAsn_y': 'peerAsnPeer'}, copy=False)
+                .explode(column="assertReason")
+                .astype({'asn': 'Int64', 'peerAsn': 'Int64', 'asnPeer': 'Int64',
+                         'peerAsnPeer': 'Int64'}, copy=False)
                 .fillna({'assertReason': '-'}))
