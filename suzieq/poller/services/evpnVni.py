d5e4e7c6924eda4fa8875663de9ee6c6c0775aa5
Dinesh Dutt <dd.ps4u@gmail.com>
2020-04-18 15:51:39 -0700

    Modify the data gathered for EVPN VNI to allow assert handling

--- suzieq/poller/services/evpnVni.py
+++ suzieq/poller/services/evpnVni.py
@@ -1,15 +1,26 @@
 from suzieq.poller.services.service import Service
+import re
 
 
 class EvpnVniService(Service):
     """evpnVni service. Different class because output needs to be munged"""
 
+    def clean_json_input(self, data):
+        """FRR JSON data needs some work"""
+
+        if data.get("devtype", None) == "cumulus":
+            data['data'] = '[' + re.sub(r'}\n\n{\n', r'},\n\n{\n',
+                                        data['data']) + ']'
+            return data['data']
+
     def clean_data(self, processed_data, raw_data):
 
         if raw_data.get("devtype", None) == "cumulus":
-            for entry in processed_data:
-                if entry["numRemoteVteps"] == "n/a":
-                    entry["numRemoteVteps"] = 0
-                if entry["remoteVteps"] == '':
-                    entry["remoteVteps"] == []
+            del_indices = []
+            for i, entry in enumerate(processed_data):
+                if entry['vni'] is None:
+                    del_indices.append(i)
+
+                for idx in del_indices:
+                    del processed_data[idx]
         return super().clean_data(processed_data, raw_data)
