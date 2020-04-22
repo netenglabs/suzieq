478cee5dbaf06d7b8efc42c77831a66271d23425
Justin Pietsch <jpietsch@gmail.com>
2020-04-22 12:41:03 -0700

    added an error for Path Unique as well as cleaned up other unique types

--- suzieq/cli/sqcmds/TableCmd.py
+++ suzieq/cli/sqcmds/TableCmd.py
@@ -71,7 +71,7 @@ class TableCmd(SqCommand):
         df = pd.DataFrame({'error': [msg]})
         return self._gen_output(df, dont_strip_cols=True)
 
-    @command("unique", help="Summarize info about this resource")
+    @command("unique", help="find the list of unique items in a column")
     def unique(self, **kwargs):
 
         msg = 'ERROR: Unique not supported for this object'
