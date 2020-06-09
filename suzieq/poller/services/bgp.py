9a607a2519a5566a6dccf6a387bebe7b78f3e485
Dinesh Dutt <dd.ps4u@gmail.com>
2020-06-09 10:40:54 -0700

    NXOS support for BGP and MAC addr

--- suzieq/poller/services/bgp.py
+++ suzieq/poller/services/bgp.py
@@ -1,4 +1,6 @@
 from suzieq.poller.services.service import Service
+from suzieq.utils import build_cisco_timestring
+from datetime import datetime
 
 
 class BgpService(Service):
@@ -6,16 +8,16 @@ class BgpService(Service):
 
     def clean_data(self, processed_data, raw_data):
 
-        processed_data = super().clean_data(processed_data, raw_data)
-
         # The AFI/SAFI key string changed in version 7.x of FRR and so we have
         # to munge the output to get the data out of the right key_fields
         if raw_data.get("devtype", None) == "eos":
             processed_data = self._clean_eos_data(processed_data, raw_data)
         elif raw_data.get("devtype", None) == "junos":
             processed_data = self._clean_junos_data(processed_data, raw_data)
+        elif raw_data.get("devtype", None) == "nxos":
+            processed_data = self._clean_nxos_data(processed_data, raw_data)
 
-        return processed_data
+        return super().clean_data(processed_data, raw_data)
 
     def _clean_eos_data(self, processed_data, raw_data):
 
@@ -91,5 +93,83 @@ class BgpService(Service):
             entry.pop('afiSafiEnabledList')
             if not entry.get('vrf', None):
                 entry['vrf'] = 'default'
+
+        return processed_data
+
+    def _clean_nxos_data(self, processed_data, raw_data):
+
+        for entry in processed_data:
+            for i, item in enumerate(entry['afiSafi']):
+                if item == 'IPv4 Unicast':
+                    entry['v4Advertised'] = entry['afAdvertised'][i]
+                    entry['v4Received'] = entry['afRcvd'][i]
+                    if (entry['v4Advertised'] == "true" and
+                            entry['v4Received'] == "true"):
+                        entry['v4Enabled'] = True
+                    else:
+                        entry['v4Enabled'] = False
+                elif item == 'IPv6 Unicast':
+                    entry['v6Advertised'] = entry['afAdvertised'][i]
+                    entry['v6Received'] = entry['afRcvd'][i]
+                    if (entry['v6Advertised'] == "true" and
+                            entry['v6Received'] == "true"):
+                        entry['v6Enabled'] = True
+                    else:
+                        entry['v6Enabled'] = False
+                elif item == 'L2VPN EVPN':
+                    entry['evpnAdvertised'] = entry['afAdvertised'][i]
+                    entry['evpnReceived'] = entry['afRcvd'][i]
+                    if (entry['evpnAdvertised'] == "true" and
+                            entry['evpnReceived'] == "true"):
+                        entry['evpnEnabled'] = True
+                    else:
+                        entry['evpnEnabled'] = False
+
+            entry.pop('afiSafi')
+            entry.pop('afAdvertised')
+            entry.pop('afRcvd')
+
+            if entry['rrclient'][0] == "true":
+                entry['rrClient'] = True
+
+            for i, item in enumerate(entry['afiPrefix']):
+                if item == 'IPv4 Unicast':
+                    entry['v4PfxRx'] = entry['pfxRcvd'][i]
+                    entry['v4PfxTx'] = entry['pfxSent'][i]
+                    entry['v4DefaultSent'] = entry['defaultOrig'][i]
+                elif item == 'IPv6 Unicast':
+                    entry['v6PfxRx'] = entry['pfxRcvd'][i]
+                    entry['v6PfxTx'] = entry['pfxSent'][i]
+                    entry['v6DefaultSent'] = entry['defaultOrig'][i]
+                elif item == 'L2VPN EVPN':
+                    entry['evpnPfxRx'] = entry['pfxRcvd'][i]
+                    entry['evpnPfxTx'] = entry['pfxSent'][i]
+                    if entry['sendComm'][i] == "true":
+                        if entry['extendComm'] == "true":
+                            entry['evpnSendCommunity'] = 'extendedAndstandard'
+                        else:
+                            entry['evpnSendCommunity'] = 'standard'
+                    elif entry['extendComm'] == "true":
+                        entry['evpnSendCommunity'] = 'extended'
+                    entry['evpnDefaultSent'] = entry['defaultOrig'][i]
+
+            entry.pop('afiPrefix')
+            entry.pop('pfxRcvd')
+            entry.pop('pfxSent')
+            entry.pop('sendComm')
+            entry.pop('extendComm')
+            entry.pop('defaultOrig')
+
+            if (entry['extnhAdvertised'] == "true" and
+                    entry['extnhReceived'] == "true"):
+                entry['extnhEnabled'] = True
+            else:
+                entry['extnhEnabled'] = False
+
+            uptime = entry['estdTime']
+            period = datetime.strptime(uptime,
+                                       build_cisco_timestring(uptime)).time()
+            secs = period.hour*3600 + period.minute*60 + period.second
+            entry['estdTime'] = raw_data['timestamp'] - secs*1000
 
         return processed_data
