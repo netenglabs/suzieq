fefd3c2e060a11be72f6494e000d73a047407b37
Justin Pietsch <jpietsch@gmail.com>
2020-06-17 05:22:18 +0000

    latest nxos/junos/eos tests update

--- tests/integration/test_sqcmds.py
+++ tests/integration/test_sqcmds.py
@@ -348,7 +348,7 @@ def test_nxos_sqcmds(testvar, create_context_config):
 @pytest.mark.sqcmds
 @pytest.mark.parametrize("testvar", load_up_the_tests(os.scandir(os.path.abspath(os.curdir) +
                         '/tests/integration/sqcmds/junos')))
-def test_nxos_sqcmds(testvar, create_context_config):
+def test_junos_sqcmds(testvar, create_context_config):
     _test_sqcmds(testvar, create_context_config)
 
 
@@ -356,5 +356,5 @@ def test_nxos_sqcmds(testvar, create_context_config):
 @pytest.mark.sqcmds
 @pytest.mark.parametrize("testvar", load_up_the_tests(os.scandir(os.path.abspath(os.curdir) +
                         '/tests/integration/sqcmds/eos')))
-def test_nxos_sqcmds(testvar, create_context_config):
+def test_eos_sqcmds(testvar, create_context_config):
     _test_sqcmds(testvar, create_context_config)
