bbd2c18ca5d4ddc9e9e41cdd2cd077447f2902e1
Justin Pietsch <jpietsch@gmail.com>
2020-06-22 17:45:22 -0700

    refactoring to deal with multiple topologies

--- suzieq/sqobjects/topology.py
+++ suzieq/sqobjects/topology.py
@@ -7,27 +7,23 @@ import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-from suzieq.sqobjects import interfaces, lldp, routes, arpnd, macs, basicobj
+from suzieq.sqobjects import interfaces, lldp, bgp, arpnd, macs, basicobj
 from suzieq.exceptions import NoLLdpError, EmptyDataframeError, PathLoopError
 
 # TODO:
-#  start with LLDP topology, which is efectively physical topology
-#  do analysis, 
-#   What is not connected to the rest of the graph
-#   what are edges to nodes we don't know about
-#  draw map
-#   
+#  how do we collect and draw multiple topologies  
 #  can we do this if there isn't LLDP?
 #  topology for different VRFs?
 #  topology for eVPN / overlay
 #  bgp topology
 #  ospf topology
+#  color by device type?
 #  be able to ask fi a node has neighbors by type (physical, overlay, protocol, etc)
 #  questions
 #    * is each topology a different command?
 #    * will we want more than one topology at a time?
 #    * without knowing hierarchy, labels or tags it's unclear how to group things
-# rather than require a namespace, do it per namespace
+
 
 
 class TopologyObj(basicobj.SqObject):
@@ -65,10 +61,6 @@ class TopologyObj(basicobj.SqObject):
         if self._if_df.empty:
             raise EmptyDataframeError(f"No interface found for {namespaces}")
 
-        self._lldp_df = lldp.LldpObj(context=self.ctxt).get(
-            namespace=namespaces, columns=self.columns)
-        if self._lldp_df.empty:
-            raise NoLLdpError(f"No LLDP information found for {namespaces}")
 
         # We ignore the lack of ARPND for now
         self._arpnd_df = arpnd.ArpndObj(
@@ -77,38 +69,60 @@ class TopologyObj(basicobj.SqObject):
         self._macsobj = macs.MacsObj(context=self.ctxt, namespace=namespaces)
     
         self._if_grp = self._if_df.groupby(by=['namespace'])
-        self._lldp_grp = self._lldp_df.groupby(by=['namespace'])
+
         self._arpnd_grp = self._arpnd_df.groupby(by=['namespace'])
     
     def get(self, **kwargs):
         namespaces = kwargs.get("namespace", self.ctxt.namespace)
+        self.graphs = {}
+        self.ns = {}
+        self._init_dfs(namespaces)
         self._create_lldp_topology(namespaces)
+        self._create_bgp_topology(namespaces)
         return pd.DataFrame(self.ns)
 
 
     def _create_lldp_topology(self, namespaces):
-        self._init_dfs(namespaces)
-        self.ns = {}
-        self.lldp_graphs = {}
-        for i in self._lldp_df['namespace'].unique():
-            self.ns[i] = {}
-            self.lldp_graphs[i] = nx.Graph(topology='LLDP', name=f"{i}") 
         
-        devices = self._lldp_grp['hostname']
+        self._lldp_df = lldp.LldpObj(context=self.ctxt).get(
+            namespace=namespaces, columns=self.columns)
+        if self._lldp_df.empty:
+            raise NoLLdpError(f"No LLDP information found")
+        self._lldp_grp = self._lldp_df.groupby(by=['namespace'])
+        self._create_topolgy_from_df(self._lldp_df, self._lldp_grp, 'LLDP')
+
+
+        
+    def _create_topolgy_from_df(self, df, group, label):
+        
+        
+        for i in df['namespace'].unique():
+            self.ns[i] = {}
+            self.graphs[i] = nx.Graph(topology=label, name=f"{i}") 
+        
+        devices = group['hostname']
 
         for ns, hosts in devices:
             hosts = hosts.unique()
             for device in hosts:
-                self.lldp_graphs[ns].add_node(device, name=device)
-                node_df = self._lldp_df.query(f"hostname == '{device}' and namespace == '{ns}'")[
+                self.graphs[ns].add_node(device, name=device)
+                node_df = df.query(f"hostname == '{device}' and namespace == '{ns}'")[
                     ['peerHostname', 'ifname']]
                 
                 for index, row in node_df.iterrows():
-                    self.lldp_graphs[ns].add_edge(device, row['peerHostname'], ifname=row['ifname'] )
+                    self.graphs[ns].add_edge(device, row['peerHostname'], ifname=row['ifname'] )
 
-        self._analyze_graph(self.lldp_graphs)
-        self._make_image(self.lldp_graphs)
-        
+        self._analyze_graph(self.graphs)
+        self._make_image(self.graphs)
+        return self.graphs
+
+    def _create_bgp_topology(self, namespaces):
+        self._bgp_df = bgp.BgpObj(context=self.ctxt).get(
+            namespace=namespaces, columns=self.columns)
+        if self._bgp_df.empty:
+            raise Exception(f"No BGP information found")
+        self.bgp_graphs = {}
+
         
     def _make_image(self, graphs):
         for name, G in graphs.items():
