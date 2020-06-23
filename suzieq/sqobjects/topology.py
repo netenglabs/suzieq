da7f70ef76c6f3bbb35410bcd09c4f1459e97f55
Justin Pietsch <jpietsch@gmail.com>
2020-06-23 15:25:56 -0700

    supports multiple topologies and added BGP

--- suzieq/sqobjects/topology.py
+++ suzieq/sqobjects/topology.py
@@ -53,7 +53,7 @@ class TopologyObj(basicobj.SqObject):
         self._cat_fields = []
 
     def _init_dfs(self, namespaces):
-        """Initialize the dataframes used in this path hunt"""
+        """Initialize the dataframes used"""
         
         self._if_df = interfaces.IfObj(context=self.ctxt) \
                                 .get(namespace=namespaces)
@@ -61,95 +61,98 @@ class TopologyObj(basicobj.SqObject):
         if self._if_df.empty:
             raise EmptyDataframeError(f"No interface found for {namespaces}")
 
-
-        # We ignore the lack of ARPND for now
-        self._arpnd_df = arpnd.ArpndObj(
-            context=self.ctxt).get(namespace=namespaces)
-
-        self._macsobj = macs.MacsObj(context=self.ctxt, namespace=namespaces)
-    
-        self._if_grp = self._if_df.groupby(by=['namespace'])
-
-        self._arpnd_grp = self._arpnd_df.groupby(by=['namespace'])
     
     def get(self, **kwargs):
         namespaces = kwargs.get("namespace", self.ctxt.namespace)
-        self.graphs = {}
+  
         self.ns = {}
         self._init_dfs(namespaces)
+        for i in self._if_df['namespace'].unique():
+            self.ns[i] = {}
+
         self._create_lldp_topology(namespaces)
         self._create_bgp_topology(namespaces)
+        
         return pd.DataFrame(self.ns)
-
+       
+    def _create_topology_from_df(self, df, group, label, key, label_col):
+        graphs = {}
+        for i in self._if_df['namespace'].unique():
+            graphs[i] = nx.Graph(name=f"{i}") 
+        devices = group['hostname']
+        
+        for ns, hosts in devices:
+            hosts = hosts.unique()
+            
+            for device in hosts:
+                graphs[ns].add_node(device, name=device)
+                # TODO: this is too iterative, I need to do this once for the namespace and then figure out how to break things up
+                node_df = df.query(f"hostname == '{device}' and namespace == '{ns}'")[
+                    [key, label_col]]
+                
+                for index, row in node_df.iterrows():
+                    graphs[ns].add_edge(device, row[key], 
+                    ifname=row[label_col], topology=label)
+        
+        self._analyze_graph(graphs, label)
+        self._make_image(graphs, label)
+        
+        return graphs
 
     def _create_lldp_topology(self, namespaces):
-        
+            
         self._lldp_df = lldp.LldpObj(context=self.ctxt).get(
             namespace=namespaces, columns=self.columns)
         if self._lldp_df.empty:
             raise NoLLdpError(f"No LLDP information found")
         self._lldp_grp = self._lldp_df.groupby(by=['namespace'])
-        self._create_topolgy_from_df(self._lldp_df, self._lldp_grp, 'LLDP')
-
-
-        
-    def _create_topolgy_from_df(self, df, group, label):
-        
-        
-        for i in df['namespace'].unique():
-            self.ns[i] = {}
-            self.graphs[i] = nx.Graph(topology=label, name=f"{i}") 
-        
-        devices = group['hostname']
-
-        for ns, hosts in devices:
-            hosts = hosts.unique()
-            for device in hosts:
-                self.graphs[ns].add_node(device, name=device)
-                node_df = df.query(f"hostname == '{device}' and namespace == '{ns}'")[
-                    ['peerHostname', 'ifname']]
-                
-                for index, row in node_df.iterrows():
-                    self.graphs[ns].add_edge(device, row['peerHostname'], ifname=row['ifname'] )
-
-        self._analyze_graph(self.graphs)
-        self._make_image(self.graphs)
-        return self.graphs
+        self._create_topology_from_df(self._lldp_df, self._lldp_grp, 
+            'LLDP', 'peerHostname', 'ifname')
 
     def _create_bgp_topology(self, namespaces):
         self._bgp_df = bgp.BgpObj(context=self.ctxt).get(
             namespace=namespaces, columns=self.columns)
         if self._bgp_df.empty:
             raise Exception(f"No BGP information found")
-        self.bgp_graphs = {}
+        self._bgp_grp = self._bgp_df.groupby(by=['namespace'])
+        
+        self._create_topology_from_df(self._bgp_df, self._bgp_grp, 'BGP',
+            'peerHostname', 'peer')
 
         
-    def _make_image(self, graphs):
-        for name, G in graphs.items():
-            nx.draw(G, with_labels=True, pos=nx.spring_layout(G))
-            
-            plt.savefig(f"{name}.png")
-            plt.close()
-
-    def _analyze_graph(self, graphs):
-        #breakpoint()
+    def _make_image(self, graphs, label):
 
         for name, G in graphs.items():
-            self.ns[name] = {}
-            if not nx.is_connected(G):
-                self.ns[name]['is_fully_connected'] = False
-                self.ns[name]['barrycenter'] = False
+            if G.nodes:
+                nx.draw(G, with_labels=True, pos=nx.spring_layout(G))
+                
+                plt.savefig(f"{label}_{name}.png")
+                plt.close()
+
+    def _analyze_graph(self, graphs, label):
+
+        for name, G in graphs.items():
+            if G.nodes:
+
+                if not nx.is_connected(G):
+                    self.ns[name][f'{label}_is_fully_connected'] = False
+                    self.ns[name][f'{label}_barrycenter'] = False
+                else:
+                    self.ns[name][f'{label}_is_fully_connected'] = True
+                    self.ns[name][f'{label}_barrycenter'] = nx.barycenter(G)
+                
+                self.ns[name][f'{label}_self_loops'] = list(nx.nodes_with_selfloops(G))
+                
+                self.ns[name][f'{label}_number_of_subgroups'] = len(list(nx.connected_components(G)))
+                
+                self.ns[name][f'{label}_degree_histogram'] = nx.degree_histogram(G)
+                #self.ns[name][f'{label}_bridges'] = list(nx.bridges(G))
             else:
-                self.ns[name]['is_fully_connected'] = True
-                self.ns[name]['barrycenter'] = nx.barycenter(G)
+                for k in [f'{label}_is_fully_connected', f'{label}_barrycenter',
+                     f'{label}_self_loops', f'{label}_number_of_subgroups', 
+                     f'{label}_degree_histogram']:
+                     self.ns[name][k] = "N/A"
             
-            self.ns[name]['self_loops'] = list(nx.nodes_with_selfloops(G))
-            
-            self.ns[name]['number_of_subgroups'] = len(list(nx.connected_components(G)))
-            
-            self.ns[name]['degree_histogram'] = nx.degree_histogram(G)
-            self.ns[name]['bridges'] = list(nx.bridges(G))
-            self.ns[name]['number_of_cycles'] = len(nx.cycle_basis(G))
         
 
             
