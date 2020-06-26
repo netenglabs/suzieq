9a8e16f7ee7d79112feaf2ad1a46f4b93a683c8e
Justin Pietsch <jpietsch@gmail.com>
2020-06-26 09:05:37 -0700

    moved ospf peerHostname agumentation to topology. totally changed topology to create a lsdb per namespace and then create graphs and images and analysis off of that.

--- suzieq/sqobjects/topology.py
+++ suzieq/sqobjects/topology.py
@@ -1,6 +1,7 @@
 import typing
 from collections import OrderedDict, defaultdict
 from itertools import repeat
+from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
@@ -8,6 +9,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 from suzieq.sqobjects import interfaces, lldp, bgp, ospf, basicobj, address
+from suzieq.sqobjects.basicobj import SqObject
 from suzieq.exceptions import NoLLdpError, EmptyDataframeError, PathLoopError
 
 # TODO:
@@ -61,6 +63,7 @@ class TopologyObj(basicobj.SqObject):
         
         if self._if_df.empty:
             raise EmptyDataframeError(f"No interface found for {namespaces}")
+        self.nses = self._if_df['namespace'].unique()
 
     
     def get(self, **kwargs):
@@ -68,136 +71,156 @@ class TopologyObj(basicobj.SqObject):
   
         self.ns = {}
         self._init_dfs(namespaces)
-        self.link_state_db = pd.DataFrame()
-        nses = self._if_df['namespace'].unique()
-        for i in nses:
+        self.lsdb = pd.DataFrame()
+
+        for i in self.nses:
             self.ns[i] = {}
 
-        services = [
-            ('LLDP', lldp.LldpObj, {}, 'peerHostname', 'ifname'),
-            ('BGP', bgp.BgpObj, {'state': 'Established'},'peerHostname', 'peer' ),
-            ('OSPF', ospf.OspfObj, {}, 'peerHostname', 'ifname')
+        self.services = [
+
+            Services('LLDP', lldp.LldpObj, {}, 'peerHostname', 'ifname', None),
+            Services('BGP', bgp.BgpObj, {'state': 'Established'},'peerHostname', 
+                'peer', None),
+            Services('OSPF', ospf.OspfObj, {}, 'peerHostname', 'ifname', 
+                self._augment_ospf_show)
             ]
-
-        self.graphs = {}
         
-        for srv, obj, extra_args, key, label_col in services:     
-            df = obj(context=self.ctxt).get(
+        for srv in self.services: 
+            
+            df = srv.data(context=self.ctxt).get(
                 namespace=namespaces, columns=self.columns,
-                **extra_args
+                **srv.extra_args
                 ).dropna(how='any')
+            
+            
             if not df.empty:
+                if srv.augment:
+                    df = srv.augment(df)
+                df = df[['namespace', 'hostname', srv.key, srv.label_col]]
+                df[srv.name] = True
+                if self.lsdb.empty:
+                    self.lsdb = df
+                else:
+                    self.lsdb = self.lsdb.merge(df, how='left')
                 grp = df.groupby(by=['namespace'])
-            
-                self.graphs[srv] = self._create_topology_from_df(df, grp, srv, key, label_col)
-            else:
-                print(f"EMPTY DF for {srv}")
-                self.graphs[srv] = self._create_empty_graphs_per_namespace(nses)
+
         
-        multigraphs = {}
-        for ns in nses:
+        self._create_graphs_from_lsdb()
+        self._analyze_lsdb_graph()
+        self._make_images()
+        # breakpoint()
+        # multigraphs = {}
+        # for ns in self.nses:
             
-            gs = []
-            edge_labels = {}
+        #     gs = []
+        #     edge_labels = {}
             
-            for srv, _, _, _, _ in services:
-                gs.append(self.graphs[srv][ns])
+        #     for srv in self.services:
+        #         gs.append(self.graphs[srv.name][ns])
                 
                 
-            G = nx.compose_all(gs)
-            for srv, _, _, _, _ in services:
-                edge_labels[srv]=nx.get_edge_attributes(self.graphs[srv][ns], 'topology')
+        #     G = nx.compose_all(gs)
+        #     for srv in self.services:
+        #         edge_labels[srv.name]=nx.get_edge_attributes(self.graphs[srv.name][ns], 'topology')
             
             
-            pos = nx.spring_layout(G)
-            nx.draw(G, with_labels=True, pos=pos)
-            colors = ['r', 'g', 'b']
-            i = 0
-            el = defaultdict(list)
-            for srv, edges in edge_labels.items():
+        #     pos = nx.spring_layout(G)
+        #     nx.draw(G, with_labels=True, pos=pos)
+        #     colors = ['r', 'g', 'b']
+        #     i = 0
+        #     el = defaultdict(list)
+        #     for srv, edges in edge_labels.items():
                 
-                for edge, _ in edges.items():
-                    el[edge].append(srv) 
-            for edge in el:
-                el[edge] = " ".join(el[edge])    
+        #         for edge, _ in edges.items():
+        #             el[edge].append(srv) 
+        #     for edge in el:
+        #         el[edge] = " ".join(el[edge])    
 
-            nx.draw_networkx_edge_labels(G, pos, edge_labels=el, font_size=6)
+        #     nx.draw_networkx_edge_labels(G, pos, edge_labels=el, font_size=6)
             
-            plt.savefig(f"{ns}.png")
-            plt.close()
+        #     plt.savefig(f"{ns}.png")
+        #     plt.close()
 
 
-            multigraphs[ns] = G
+        #     multigraphs[ns] = G
         return pd.DataFrame(self.ns)
 
-    def _create_empty_graphs_per_namespace(self, namespaces):
-        graphs = {}
-        for ns in namespaces:
-            graphs[ns] = nx.Graph()
-        return graphs
-       
-    def _create_topology_from_df(self, df, group, label, key, label_col):
-        graphs = {}
-        for i in self._if_df['namespace'].unique():
-            graphs[i] = nx.Graph(name=f"{i}") 
-        devices = group['hostname']
-        
-        for ns, hosts in devices:
-            hosts = hosts.unique()
-            
-            for device in hosts:
-                graphs[ns].add_node(device, name=device)
-                # TODO: this is too iterative, I need to do this once for the namespace and then figure out how to break things up
-                node_df = df.query(f"hostname == '{device}' and namespace == '{ns}'")[
-                    [key, label_col]]
+ 
+    def _create_graphs_from_lsdb(self):
+        self.graphs = {}
+        for ns, df in self.lsdb.groupby(by=['namespace']):
+            attrs = [srv.name for srv in self.services if srv.name in df.columns]
+            self.graphs[ns] = nx.from_pandas_edgelist(df, 'hostname', 'peerHostname', attrs)
+
+    def _analyze_lsdb_graph(self):
+        for ns in self.nses:
+            for name in [srv.name for srv in self.services if \
+                nx.get_edge_attributes(self.graphs[ns], srv.name)]:
                 
-                for index, row in node_df.iterrows():
-                    graphs[ns].add_edge(device, row[key], 
-                    topology=label)
-        
-        self._analyze_graph(graphs, label)
-        self._make_image(graphs, label)
-        
-        return graphs
-
-
-    def _make_image(self, graphs, label):
-
-        for name, G in graphs.items():
-            if G.nodes:
-                nx.draw(G, with_labels=True, pos=nx.spring_layout(G))
-                
-                plt.savefig(f"{label}_{name}.png")
-                plt.close()
-
-    def _analyze_graph(self, graphs, label):
-
-        for name, G in graphs.items():
-            if G.nodes:
-                self.ns[name][f'{label}_number_of_nodes'] = len(G.nodes)
-                self.ns[name][f'{label}_number_of_edges'] = len(G.edges)
-                if not nx.is_connected(G):
-                    self.ns[name][f'{label}_is_fully_connected'] = False
-                    self.ns[name][f'{label}_center'] = False
+                G = nx.Graph([(s,d,data) for s,d,data in 
+                    self.graphs[ns].edges(data=True) if data[name] == True])
+                if G.nodes:
+                    self.ns[ns][f'{name}_number_of_nodes'] = len(G.nodes)
+                    self.ns[ns][f'{name}_number_of_edges'] = len(G.edges)
+                    if not nx.is_connected(G):
+                        self.ns[ns][f'{name}_is_fully_connected'] = False
+                        self.ns[ns][f'{name}_center'] = False
+                    else:
+                        self.ns[ns][f'{name}_is_fully_connected'] = True
+                        self.ns[ns][f'{name}_center'] = nx.barycenter(G)
+                    
+                    self.ns[ns][f'{name}_self_loops'] = list(nx.nodes_with_selfloops(G))
+                    
+                    self.ns[ns][f'{name}_number_of_subgroups'] = len(list(nx.connected_components(G)))
+                    
+                    self.ns[ns][f'{name}_degree_histogram'] = nx.degree_histogram(G)
+                    #self.ns[ns][f'{name}_bridges'] = list(nx.bridges(G))
                 else:
-                    self.ns[name][f'{label}_is_fully_connected'] = True
-                    self.ns[name][f'{label}_center'] = nx.barycenter(G)
+                    for k in [f'{name}_is_fully_connected', f'{name}_center',
+                        f'{name}_self_loops', f'{name}_number_of_subgroups', 
+                        f'{name}_degree_histogram', f'{name}_number_of_nodes',
+                        f'{name}_number_of_edges']:
+                        self.ns[ns][k] = "N/A"
+
+
+    def _make_images(self):
+        for ns in self.nses:
+            pos = nx.spring_layout(self.graphs[ns])
+            for name in [srv.name for srv in self.services if \
+                nx.get_edge_attributes(self.graphs[ns], srv.name)]: 
+                edges = [(u,v) for u,v,d in self.graphs[ns].edges(data=True) if \
+                    d[name] == True]
+                if len(edges) > 1:    
                 
-                self.ns[name][f'{label}_self_loops'] = list(nx.nodes_with_selfloops(G))
-                
-                self.ns[name][f'{label}_number_of_subgroups'] = len(list(nx.connected_components(G)))
-                
-                self.ns[name][f'{label}_degree_histogram'] = nx.degree_histogram(G)
-                #self.ns[name][f'{label}_bridges'] = list(nx.bridges(G))
-            else:
-                for k in [f'{label}_is_fully_connected', f'{label}_center',
-                     f'{label}_self_loops', f'{label}_number_of_subgroups', 
-                     f'{label}_degree_histogram', f'{label}_number_of_nodes',
-                     f'{label}_number_of_edges']:
-                     self.ns[name][k] = "N/A"
+                    nx.draw_networkx_nodes(self.graphs[ns], pos=pos)
+                    nx.draw_networkx_labels(self.graphs[ns], pos=pos)
+                    
+                    nx.draw_networkx_edges(self.graphs[ns], edgelist = edges, pos=pos)
+                    plt.savefig(f"{ns}_{name}.png")
+                    plt.close()
+
+    # TODO: eventually this needs to move to ospf after we figure out the schema augmentation story
+    def _augment_ospf_show(self, df):
+        if not df.empty:
+            a_df = address.AddressObj(context=self.ctxt).get(columns=self.columns)
             
+            if not a_df.empty:
+                a_df = a_df[['namespace', 'hostname', 'ipAddressList']]
+                a_df = a_df.explode('ipAddressList').dropna(how='any')
+                a_df = a_df.rename(columns={'ipAddressList': 'peerIP', 
+                'hostname': 'peerHostname'})
+                a_df['peerIP'] = a_df['peerIP'].str.replace("/.+", "")
+
+                df = df.merge(a_df, on=['namespace', 'peerIP'], how='left').dropna(how='any')
+
+        return df
+
         
-
-            
-
-
+@dataclass(frozen=True)
+class Services:
+    name: str
+    data: SqObject
+    extra_args: dict
+    key: str
+    label_col: str
+    augment: any
