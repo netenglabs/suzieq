08713736592d1d13f3ed48e9030a4f67ef162ea4
root <andrea@opensuse.org>
2020-07-28 11:04:26 +0200

    add sqobjects and make cli meaningful

---
+++ suzieq/sqobjects/fs.py
@@ -0,0 +1,16 @@
+import typing
+
+from suzieq.sqobjects import basicobj
+
+
+class FsObj(basicobj.SqObject):
+
+    def __init__(self, engine: str = '', hostname: typing.List[str] = [],
+                 start_time: str = '', end_time: str = '',
+                 view: str = 'latest', namespace: typing.List[str] = [],
+                 columns: typing.List[str] = ['default'],
+                 context=None) -> None:
+        super().__init__(engine, hostname, start_time, end_time, view,
+                         namespace, columns, context=context, table='fs')
+        self._sort_fields = ['namespace', 'hostname']
+        self._cat_fields = []
