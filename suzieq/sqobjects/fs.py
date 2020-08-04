feed462f311958202f6fcf3cf601f19be0a2be55
Dinesh Dutt <dd.ps4u@gmail.com>
2020-08-03 23:19:12 -0700

    Cleanup FSCmd support incl add JunOS support

--- suzieq/sqobjects/fs.py
+++ suzieq/sqobjects/fs.py
@@ -14,5 +14,5 @@ class FsObj(basicobj.SqObject):
                          namespace, columns, context=context, table='fs')
         self._sort_fields = ['namespace', 'hostname', 'device']
         self._cat_fields = []
-        self._ign_key_fields = ["device"]  # device is not a partition tool
-        self._ign_key_fields = ["mountPoint"]  # mountPoint is not a partition tool
+        # mountPoint is not a partition key
+        self._ign_key_fields = ["mountPoint"]
