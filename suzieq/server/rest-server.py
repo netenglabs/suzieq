1618c36f447b2fe554380ad27776675fbcec72ef
Justin Pietsch <jpietsch@gmail.com>
2020-09-14 17:06:48 -0700

    generic support for services and verbs. lots of things don't work, but more things work than before

--- suzieq/server/rest-server.py
+++ suzieq/server/rest-server.py
@@ -1,31 +1,50 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import logging 
+import uuid
 
 from suzieq.sqobjects import *
 
 
 app = FastAPI()
+# TODO: logging to this file isn't working
+logging.FileHandler('/tmp/rest-server.log')
+logger = logging.getLogger(__name__)
 
-
-@app.get("/v1//table/show")
-async def read_table_show():
-    return await read_table('get')
-@app.get("/v1/table/{verb}")
-async def read_table(verb: str):
-    return getattr(tables.TablesObj(), verb)().to_json(orient="records")
-
-@app.get("/v1/device/show")
-async def read_device_show():
-    return await read_device('get')
-@app.get("/v1/device/{verb}")
-async def read_device(verb: str, hostname: str = "",
+@app.get("/api/v1/{service}/{verb}")
+async def read_service(service: str, verb: str, hostname: str = "",
                       start_time: str = "", end_time: str = "",
                       view: str = "latest", namespace: str = ""):
-    return getattr(device.DeviceObj(hostname=hostname,
+    if verb == 'show':
+        verb = 'get'
+
+    try: 
+        module = globals()[service]
+    except KeyError:
+        service = f"{service}s"
+        module = globals()[service]
+
+    svc = getattr(module, f"{service.title()}Obj")
+
+    try:
+
+        return getattr(svc(hostname=hostname,
                                     start_time=start_time,
                                     end_time=end_time,
                                     view=view,
                                     namespace=namespace), verb)\
                                     (hostname=hostname, 
                                     namespace=namespace)\
-                                    .to_json(orient="records")
+                                    .to_json(orient="records")    
+
+     # TODO: why can't i catch NotImplemented? that's what I want here?
+    except AttributeError as err:
+        u = uuid.uuid1()
+        logger.warning(f"{verb} not supported for {service}: {err} id={u}")
+        raise HTTPException(status_code=404,
+                            detail=f"{verb} not supported for {service}: {err} id={u}")
+    except Exception as err:
+        u = uuid.uuid1()
+        logger.warning(f"exceptional exception {verb} for {service}: {err} id={u}")
+        raise HTTPException(status_code=404,
+                            detail=f"exceptional exception {verb} for {service}: {err} id={u}")         
