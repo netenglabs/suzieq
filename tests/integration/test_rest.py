8f77f444a7527ba3d1f85e5620292a8fe42db4fe
Justin Pietsch <jpietsch@gmail.com>
2020-09-22 16:48:30 -0700

    broke out all the rest tests and added more tests that should be returning 404 so they won't fail

--- tests/integration/test_rest.py
+++ tests/integration/test_rest.py
@@ -9,7 +9,7 @@ from tests.conftest import cli_commands, tables, setup_sqcmds
 # launch uvicorn for localhost and test against it
 # figure out how to make localhost work, instead of a specific IP
 
-ENDPOINT = "http://192.168.1.185:8000/api/v1"
+ENDPOINT = "http://localhost:8000/api/v1"
 
  # these service/verb pairs should return errors
 bad_verbs = {'address/assert': 404, 'address/lpm': 404,
@@ -22,6 +22,7 @@ bad_verbs = {'address/assert': 404, 'address/lpm': 404,
              'mac/assert': 404, 'mac/lpm': 404,
              'mlag/assert': 404, 'mlag/lpm': 404,
              'ospf/lpm': 404,
+             'path/assert': 404,
              'path/lpm': 404,
              'route/assert': 404, 
              'vlan/assert': 404, 'vlan/lpm': 404,
@@ -30,25 +31,32 @@ bad_verbs = {'address/assert': 404, 'address/lpm': 404,
 # these service/verb/filter tuples should return errors
 bad_filters = {'lpm?hostname=leaf01':404,
 
-               'path/show?': 404, 'path/show?hostname=leaf01': 404, 'path/show?namespace=dual-bgp': 404,
-               'path/show?address=10.0.0.1': 404,
-               'route/lpm?': 404, 'route/lpm?hostname=leaf01': 404, 'route/lpm?namespace=dual-bgp': 404,
+                'path/show?': 404, 'path/show?hostname=leaf01': 404, 'path/show?namespace=dual-bgp': 404,
+                'path/show?address=10.0.0.1': 404,
+                'path/summarize?': 404, 'path/summarize?hostname=leaf01': 404, 'path/summarize?namespace=dual-bgp': 404,
+                'path/summarize?address=10.0.0.1': 404,
+                'route/lpm?': 404, 'route/lpm?hostname=leaf01': 404, 'route/lpm?namespace=dual-bgp': 404,
+                'route/lpm?dest=172.16.2.104&src=172.16.1.101&namespace=dual-evpn': 404,
                }
 
 def get(endpoint, command, verb, args):
     url = f"{endpoint}/{command}/{verb}?{args}"
    
     ret = requests.get(url)
-    #print(f"{url}: {ret.status_code}")
+    
+    c_v = f"{command}/{verb}"
+    c_v_f = f"{c_v}?{args}"
     if ret.status_code != 200:
-        c_v = f"{command}/{verb}"
-        c_v_f = f"{c_v}?{args}"
+        
         if c_v in bad_verbs:
              assert bad_verbs[c_v] == ret.status_code
         elif c_v_f in bad_filters:
             assert bad_filters[c_v_f] == ret.status_code    
         else:
             ret.raise_for_status()
+    else:
+        assert c_v not in bad_verbs
+        assert c_v_f not in bad_filters
 
     return ret.status_code
 
@@ -58,17 +66,13 @@ FILTERS = ['', 'hostname=leaf01', 'namespace=dual-bgp',
            'dest=172.16.2.104&src=172.16.1.101&namespace=dual-evpn',
           ]
 
-@pytest.mark.parametrize("command, verbs, args", [
-    (cmd, VERBS, FILTERS) for cmd in cli_commands
+@pytest.mark.parametrize("command, verb, arg", [
+    (cmd, verb, filter) for cmd in cli_commands \
+                         for verb in VERBS for filter in FILTERS
 ])
-def test_rest_commands(setup_nubia, command, verbs, args):
-    for verb in verbs:
-        for arg in args:
-           get(ENDPOINT, command, verb, arg)
+def test_rest_commands(setup_nubia, command, verb, arg):
+    get(ENDPOINT, command, verb, arg)
             
 
 def test_bad_rest():
-    pass
-
-if __name__ == "__main__":
     pass
