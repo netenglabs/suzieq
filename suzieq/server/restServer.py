cb5785501510dbb9f02ea953286c188f9d5eef28
Justin Pietsch <jpietsch@gmail.com>
2020-10-01 10:52:08 -0700

    pep8, changed general exceptions to return 405

--- suzieq/server/restServer.py
+++ suzieq/server/restServer.py
@@ -1,15 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI, HTTPException
-import logging 
+import logging
 import uuid
 import uvicorn
 import argparse
-import os
 import sys
 import yaml
 
 from suzieq.sqobjects import *
-from suzieq.utils import load_sq_config, validate_sq_config
+from suzieq.utils import validate_sq_config
 
 app = FastAPI()
 
@@ -17,26 +16,27 @@ app = FastAPI()
 logging.FileHandler('/tmp/rest-server.log')
 logger = logging.getLogger(__name__)
 
+
 @app.get("/api/v1/path/{verb}")
 async def read_path(verb: str, namespace: str = "", start_time: str = "",
                     end_time: str = "", src: str = "", dest: str = "",
-                    vrf: str  = "", view: str = 'latest'):
+                    vrf: str = "", view: str = 'latest'):
     namespace = namespace.split()
-    if verb == 'show':
-        verb = 'get'
+    verb = cleanup_verb(verb)
     service_args = create_service_args('', start_time, end_time, view, namespace)
 
     verb_args = {'namespace': namespace,
                  'source': src,
                  'dest': dest,
                  'vrf': vrf}
-    return run_service_verb('path', verb,  service_args, verb_args)                    
+    return run_service_verb('path', verb,  service_args, verb_args)
+
 
 @app.get("/api/v1/route/lpm")
 async def read_route_lpm(address: str = "",
-                          hostname: str= "", start_time: str = "",
-                          end_time: str = "", view: str = "latest",
-                          namespace: str = "", ):
+                         hostname: str = "", start_time: str = "",
+                         end_time: str = "", view: str = "latest",
+                         namespace: str = "", ):
     service_args = create_service_args(hostname, start_time, end_time, view, namespace)
 
     verb_args = {'hostname': hostname,
@@ -44,48 +44,55 @@ async def read_route_lpm(address: str = "",
                  'address': address}
     return run_service_verb('route', 'lpm',  service_args, verb_args)
 
+
 @app.get("/api/v1/{service}/{verb}")
 async def read_service(service: str, verb: str, hostname: str = "",
-                      start_time: str = "", end_time: str = "",
-                      view: str = "latest", namespace: str = ""):
+                       start_time: str = "", end_time: str = "",
+                       view: str = "latest", namespace: str = ""):
     """
     Get data from **service** and **verb**
-    
+
     - allows filters of **hostname**, **namespace**, **start_time**, **end_time**, and **view**
     """
+    verb = cleanup_verb(verb)
+    service_args = create_service_args(hostname, start_time, end_time, view, namespace)
+
+    verb_args = {'hostname': hostname,
+                 'namespace': namespace}
+    if verb == 'summarize':
+        verb_args.pop('hostname', None)
+
+    return run_service_verb(service, verb, service_args, verb_args)
+
+
+def cleanup_verb(verb):
     if verb == 'show':
         verb = 'get'
     if verb == 'assert':
         verb = 'aver'
+    return verb
 
-    service_args = create_service_args(hostname, start_time, end_time, view, namespace)
-
-    verb_args = {'hostname': hostname,
-                'namespace': namespace}
-    if verb == 'summarize':
-        verb_args.pop('hostname', None)
-    
-    return run_service_verb(service, verb, service_args, verb_args)
 
 def create_service_args(hostname, start_time, end_time, view, namespace):
     service_args = {'hostname': hostname,
-                'start_time': start_time,
-                'end_time': end_time,
-                'view': view,
-                'namespace': namespace}
+                    'start_time': start_time,
+                    'end_time': end_time,
+                    'view': view,
+                    'namespace': namespace}
     return service_args
+
 
 def get_svc(service):
     service_name = service
 
-    # we almost have a consistent naming scheme, but not quite. 
+    # we almost have a consistent naming scheme, but not quite.
     # sometime there are s at the end and sometimes not
-    try: 
+    try:
         module = globals()[service]
     except KeyError:
         service = f"{service}s"
         module = globals()[service]
-    
+
     try:
         svc = getattr(module, f"{service.title()}Obj")
     except AttributeError:
@@ -96,26 +103,26 @@ def get_svc(service):
             svc = getattr(module, f"{service_name.title()}Obj")
     return svc
 
+
 def run_service_verb(service, verb, service_args, verb_args):
     svc = get_svc(service)
     try:
-        print(f'app.cfg_file: {app.cfg_file}')
-        return getattr(svc(**service_args, config_file=app.cfg_file), verb)\
-                            (**verb_args).to_json(orient="records")
-    
+        return getattr(svc(**service_args, config_file=app.cfg_file), verb)(**verb_args).to_json(orient="records")
+
     except AttributeError as err:
         u = uuid.uuid1()
         msg = f"{verb} not supported for {service} or missing arguement: {err} id={u}"
         logger.warning(msg)
-        raise HTTPException(status_code=404, 
+        raise HTTPException(status_code=404,
                             detail=msg)
-    # TODO: why can't i catch NotImplemented? that's what I want here. 
+    # TODO: why can't i catch NotImplemented? that's what I want here.
     except Exception as err:
         u = uuid.uuid1()
         msg = f"exceptional exception {verb} for {service}: {err} id={u}"
         logger.warning(msg)
-        raise HTTPException(status_code=404, 
+        raise HTTPException(status_code=405,
                             detail=msg)
+
 
 @app.get("/api/v1/{service}")
 def missing_verb(service):
@@ -124,6 +131,7 @@ def missing_verb(service):
     logger.warning(msg)
     raise HTTPException(status_code=404, detail=msg)
 
+
 @app.get("/")
 def bad_path():
     u = uuid.uuid1()
@@ -131,12 +139,14 @@ def bad_path():
     logger.warning(msg)
     raise HTTPException(status_code=404, detail=msg)
 
+
 def check_config_file(cfgfile):
     if cfgfile:
         with open(cfgfile, "r") as f:
             cfg = yaml.safe_load(f.read())
 
         validate_sq_config(cfg, sys.stderr)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -148,5 +158,5 @@ if __name__ == "__main__":
     userargs = parser.parse_args()
     check_config_file(userargs.config)
     app.cfg_file = userargs.config
-  
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- tests/integration/test_rest.py
+++ tests/integration/test_rest.py
@@ -13,55 +13,55 @@ from suzieq.server.restServer import app
 ENDPOINT = "http://localhost:8000/api/v1"
 
 VERBS = ['show', 'summarize', 'assert', 'lpm', 'top']
-FILTERS = ['', 'hostname=leaf01', 'namespace=dual-bgp', 
-           'address=10.0.0.1', 
+FILTERS = ['', 'hostname=leaf01', 'namespace=dual-bgp',
+           'address=10.0.0.1',
            'dest=172.16.2.104&src=172.16.1.101&namespace=dual-evpn',
-          ]
+           ]
 
- # these service/verb pairs should return errors
-BAD_VERBS = {'address/assert': 404, 'address/lpm': 404,
-             'arpnd/assert': 404, 'arpnd/lpm': 404,
+# these service/verb pairs should return errors
+BAD_VERBS = {'address/assert': 405, 'address/lpm': 404,
+             'arpnd/assert': 405, 'arpnd/lpm': 404,
              'bgp/lpm': 404,
-             'device/assert': 404, 'device/lpm': 404,
+             'device/assert': 405, 'device/lpm': 404,
              'evpnVni/lpm': 404,
              'interface/lpm': 404,
-             'lldp/assert': 404, 'lldp/lpm': 404,               
-             'mac/assert': 404, 'mac/lpm': 404,
-             'mlag/assert': 404, 'mlag/lpm': 404,
+             'lldp/assert': 405, 'lldp/lpm': 404,
+             'mac/assert': 405, 'mac/lpm': 404,
+             'mlag/assert': 405, 'mlag/lpm': 404,
              'ospf/lpm': 404,
-             'path/assert': 404,
+             'path/assert': 405,
              'path/lpm': 404,
-             'route/assert': 404, 
-             'vlan/assert': 404, 'vlan/lpm': 404,
-            }
+             'route/assert': 405,
+             'vlan/assert': 405, 'vlan/lpm': 404,
+             }
 
 # these service/verb/filter tuples should return errors
-BAD_FILTERS = {'lpm?hostname=leaf01':404,
-
-                'path/show?': 404, 'path/show?hostname=leaf01': 404, 
-                'path/show?namespace=dual-bgp': 404,
-                'path/show?address=10.0.0.1': 404,
-                'path/summarize?': 404, 'path/summarize?hostname=leaf01': 404, 
-                'path/summarize?namespace=dual-bgp': 404,
-                'path/summarize?address=10.0.0.1': 404,
-                'route/lpm?': 404, 'route/lpm?hostname=leaf01': 404, 
-                'route/lpm?namespace=dual-bgp': 404,
-                'route/lpm?dest=172.16.2.104&src=172.16.1.101&namespace=dual-evpn': 404,
+BAD_FILTERS = {'lpm?hostname=leaf01': 404,
+               'path/show?': 404, 'path/show?hostname=leaf01': 404,
+               'path/show?namespace=dual-bgp': 404,
+               'path/show?address=10.0.0.1': 404,
+               'path/summarize?': 404, 'path/summarize?hostname=leaf01': 404,
+               'path/summarize?namespace=dual-bgp': 404,
+               'path/summarize?address=10.0.0.1': 404,
+               'route/lpm?': 404, 'route/lpm?hostname=leaf01': 404,
+               'route/lpm?namespace=dual-bgp': 404,
+               'route/lpm?dest=172.16.2.104&src=172.16.1.101&namespace=dual-evpn': 404,
                }
+
 
 def get(endpoint, command, verb, args):
     url = f"{endpoint}/{command}/{verb}?{args}"
-   
+
     ret = requests.get(url)
-    
+
     c_v = f"{command}/{verb}"
     c_v_f = f"{c_v}?{args}"
     if ret.status_code != 200:
-        
+
         if c_v in BAD_VERBS:
-             assert BAD_VERBS[c_v] == ret.status_code
+            assert BAD_VERBS[c_v] == ret.status_code
         elif c_v_f in BAD_FILTERS:
-            assert BAD_FILTERS[c_v_f] == ret.status_code    
+            assert BAD_FILTERS[c_v_f] == ret.status_code
         else:
             ret.raise_for_status()
     else:
@@ -72,17 +72,18 @@ def get(endpoint, command, verb, args):
 
 
 @pytest.mark.parametrize("command, verb, arg", [
-    (cmd, verb, filter) for cmd in cli_commands \
-                         for verb in VERBS for filter in FILTERS
+    (cmd, verb, filter) for cmd in cli_commands
+    for verb in VERBS for filter in FILTERS
 ])
 def test_rest_commands(setup_nubia, start_server, command, verb, arg):
     get(ENDPOINT, command, verb, arg)
+
 
 def create_config():
     # We need to create a tempfile to hold the config
     tmpconfig = conftest._create_context_config()
 
-    tmpconfig['data-directory'] =  './tests/data/multidc/parquet-out'
+    tmpconfig['data-directory'] = './tests/data/multidc/parquet-out'
     r_int = random.randint(17, 2073)
     fname = f'/tmp/suzieq-cfg-{r_int}.yml'
 
@@ -90,16 +91,17 @@ def create_config():
         f.write(yaml.dump(tmpconfig))
     return fname
 
+
 @pytest.fixture(scope="session")
 def start_server():
-    time.sleep(0.3)
-    
     app.cfg_file = create_config()
 
-    Process(target=uvicorn.run, 
+    Process(target=uvicorn.run,
             args=(app,),
             kwargs={'host': '0.0.0.0', 'port': 8000},
-            daemon=True).start()   
+            daemon=True).start()
+    time.sleep(0.3)
+
 
 def test_bad_rest():
     pass
