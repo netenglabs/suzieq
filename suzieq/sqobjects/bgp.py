a170dd78d2f01402744af0f1dfff66bbf0f6d810
Justin Pietsch <jpietsch@gmail.com>
2020-10-07 16:16:57 -0700

    putting state back for bgp show args

--- suzieq/sqobjects/bgp.py
+++ suzieq/sqobjects/bgp.py
@@ -5,7 +5,7 @@ import pandas as pd
 class BgpObj(SqObject):
     def __init__(self, **kwargs):
         super().__init__(table='bgp', **kwargs)
-        self._valid_get_args = ['namespace', 'hostname',
+        self._valid_get_args = ['namespace', 'hostname', 'state',
                                 'status', 'vrf', 'peer'] 
         self._valid_assert_args = ['namespace', 'hostname', 'start_time',
                                   'end_time', 'vrf', ]
