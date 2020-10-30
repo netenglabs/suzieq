e06960a471bd812350af0cb340fcdac07a71b4e2
Dinesh Dutt <dd.ps4u@gmail.com>
2020-10-30 15:28:16 -0700

    Fixes to VLAN resource given recent changes

--- suzieq/engines/pandas/vlan.py
+++ suzieq/engines/pandas/vlan.py
@@ -5,51 +5,14 @@ from .engineobj import SqEngineObject
 
 class VlanObj(SqEngineObject):
 
-    def get(self, **kwargs) -> pd.DataFrame:
-        """Retrieve the dataframe that matches a given VLANs"""
-
-        if not self.iobj._table:
-            raise NotImplementedError
-
-        # Our query string formatter doesn't understand handling queries
-        # with lists yet. So, pull it out.
-        vlan = kwargs.get('vlan', None)
-        if vlan:
-            del kwargs['vlan']
-            vset = set([int(x) for x in vlan.split()])
-        else:
-            vset = None
-
-        df = self.get_valid_df("vlan", **kwargs)
-
-        # We're checking if any element of given list is in the vlan
-        # column
-        if vset:
-            return (df[df.vlan.map(lambda x: not vset.isdisjoint(x))])
-        else:
-            return(df)
-
     def summarize(self, **kwargs):
         """Describe the IP Address data"""
 
-        self._init_summarize(self.iobj._table, **kwargs)
+        self._init_summarize(self.iobj.table, **kwargs)
         if self.summary_df.empty:
             return self.summary_df
 
         self._add_field_to_summary('hostname', 'count', 'deviceCnt')
-        for field in ['pvid']:
-            self._add_list_or_count_to_summary(
-                field, field_name='uniquePvidCnt')
-
-        # To summarize accurately, we need to explode the vlan
-        # column from a list to an individual entry for each
-        # vlan in that list. The resulting set can be huge if them
-        # number of vlans times the ports is huge.
-        #
-        # the 'explode' only works post pandas 0.25
-
-        self.summary_df = self.summary_df.explode('vlan').dropna(how='any')
-        self.nsgrp = self.summary_df.groupby(by=["namespace"])
 
         if not self.summary_df.empty:
             for field in ['vlan']:
