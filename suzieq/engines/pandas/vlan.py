dbcbac645670167027e0a2b65b91d44eded3ad0d
Dinesh Dutt <dd.ps4u@gmail.com>
2020-11-23 07:29:03 -0800

    VLAN resource updates: rename ifname to vlanName, preserve it

--- suzieq/engines/pandas/vlan.py
+++ suzieq/engines/pandas/vlan.py
@@ -1,9 +1,33 @@
-import pandas as pd
+import numpy as np
 
 from .engineobj import SqEngineObject
 
 
 class VlanObj(SqEngineObject):
+
+    def get(self, **kwargs):
+        """Get VLAN info based on specified keywords"""
+
+        # ifname is a deprecated field, from version 1.0
+        # vlanName is the correct fieldname. SO we need to do magic
+        # to fix this. And thats why this routine exists
+
+        dropcols = []
+        addnl_fields = kwargs.pop('addnl_fields', [])
+        columns = kwargs.pop('columns', [])
+        if (columns != ['*'] and (columns == ['default'] or
+                                  'ifname' not in columns)):
+            if 'ifname' not in addnl_fields:
+                addnl_fields.append('ifname')
+                dropcols.append('ifname')
+
+        df = super().get(addnl_fields=addnl_fields,
+                         merge_fields={'ifname': 'vlanName'},
+                         **kwargs)
+        if not df.empty:
+            df.drop(columns=dropcols, errors='ignore', inplace=True)
+
+        return df
 
     def summarize(self, **kwargs):
         """Describe the IP Address data"""
