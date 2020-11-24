netenglabs/suzieq

commit 3591f348e3639e19b8970ab9e3b47ef335fec61c

routes: Allow summarize to work with specific host

suzieq/cli/sqcmds/RouteCmd.py
@@ -88,6 +88,33 @@ class RouteCmd(SqCommand):
         self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
         return self._gen_output(df)
 
+    @command("summarize")
+    @argument("vrf", description="VRF to qualify")
+    def show(self, vrf: str = ''):
+        """
+        Show Routes info
+        """
+        if self.columns is None:
+            return
+
+        # Get the default display field names
+        now = time.time()
+
+        if self.columns != ["default"]:
+            self.ctxt.sort_fields = None
+        else:
+            self.ctxt.sort_fields = []
+
+        df = self.sqobj.get(
+            hostname=self.hostname,
+            vrf=vrf.split(),
+            columns=self.columns,
+            namespace=self.namespace,
+        )
+
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+        return self._gen_output(df)
+
     @command('lpm')
     @argument("address", description="IP Address, in quotes, for lpm query")
     @argument("vrf", description="specific VRF to qualify")

suzieq/sqobjects/routes.py
@@ -26,5 +26,5 @@ class RoutesObj(SqObject):
             self.summarize_df = pd.DataFrame(
                 {'error': ['ERROR: You cannot specify columns with summarize']})
             return self.summarize_df
-        return self.engine_obj.summarize(namespace=namespace,
-                                         hostname=hostname, vrf=vrf)
+        return self.engine_obj.summarize(namespace=namespace, vrf=vrf,
+                                         hostname=hostname)

commit 13333ed26162d50eaa5d6d7089d10c967e84e330

RouteCmd: Fix regression introduced by commit 3591f348e3639e19b8970ab9e3b47ef335fec61c
That commit introduced a bad bug aliasing summarize and show commands.
This commit fixes that.

suzieq/cli/sqcmds/RouteCmd.py
@@ -90,30 +90,21 @@ class RouteCmd(SqCommand):
 
     @command("summarize")
     @argument("vrf", description="VRF to qualify")
-    def show(self, vrf: str = ''):
+    def summarize(self, vrf: str = ''):
         """
         Show Routes info
         """
-        if self.columns is None:
-            return
-
         # Get the default display field names
         now = time.time()
 
-        if self.columns != ["default"]:
-            self.ctxt.sort_fields = None
-        else:
-            self.ctxt.sort_fields = []
-
-        df = self.sqobj.get(
+        df = self.sqobj.summarize(
             hostname=self.hostname,
             vrf=vrf.split(),
-            columns=self.columns,
             namespace=self.namespace,
         )
 
         self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
-        return self._gen_output(df)
+        return self._gen_output(df, json_orient='columns')
 
     @command('lpm')
     @argument("address", description="IP Address, in quotes, for lpm query")