e401dd1587a125f08ff964bf5ad7d160a1da6ef2
Dinesh Dutt <dd.ps4u@gmail.com>
2020-12-06 22:06:34 -0800

    Check that the data is there before trying to display

--- suzieq/gui/pages/status.py
+++ suzieq/gui/pages/status.py
@@ -62,84 +62,102 @@ def page_work(state_container: SessionState, page_flip: bool = False):
     bgp_df = gui_get_df(state_container.sqobjs['bgp'], columns=['*'])
     ospf_df = gui_get_df(state_container.sqobjs['ospf'], columns=['*'])
 
-    if_df['state'] = np.where((if_df.state == "down") & (
-        if_df.adminState == "down"), "adminDown", if_df.state)
-    ospf_df['state'] = np.where(ospf_df.ifState == "adminDown", "adminDown",
-                                ospf_df.adjState)
+    if not if_df.empty:
+        if_df['state'] = np.where((if_df.state == "down") & (
+            if_df.adminState == "down"), "adminDown", if_df.state)
+    if not ospf_df.empty:
+        ospf_df['state'] = np.where(ospf_df.ifState == "adminDown", "adminDown",
+                                    ospf_df.adjState)
 
-    dev_status = dev_df.groupby(by=['namespace', 'status'])['hostname'] \
-                       .count() \
-                       .reset_index() \
-                       .rename({'hostname': 'count'}, axis=1)
-    if_status = if_df.groupby(by=['namespace', 'state'])['hostname'] \
-                     .count() \
-                     .reset_index() \
-                     .rename({'hostname': 'count'}, axis=1)
-    bgp_status = bgp_df.groupby(by=['namespace', 'state'])['hostname'] \
-                       .count() \
-                       .reset_index() \
-                       .rename({'hostname': 'count'}, axis=1)
-
-    ospf_status = ospf_df.groupby(by=['namespace', 'state'])['hostname'] \
+    if not dev_df.empty:
+        dev_status = dev_df.groupby(by=['namespace', 'status'])['hostname'] \
+                           .count() \
+                           .reset_index() \
+                           .rename({'hostname': 'count'}, axis=1)
+    if not if_df.empty:
+        if_status = if_df.groupby(by=['namespace', 'state'])['hostname'] \
                          .count() \
                          .reset_index() \
                          .rename({'hostname': 'count'}, axis=1)
+    if not bgp_df.empty:
+        bgp_status = bgp_df.groupby(by=['namespace', 'state'])['hostname'] \
+                           .count() \
+                           .reset_index() \
+                           .rename({'hostname': 'count'}, axis=1)
+
+    if not ospf_df.empty:
+        ospf_status = ospf_df.groupby(by=['namespace', 'state'])['hostname'] \
+                             .count() \
+                             .reset_index() \
+                             .rename({'hostname': 'count'}, axis=1)
 
     container_1 = st.beta_container()
     col1, mid, col2 = st.beta_columns([2, 1, 2])
 
-    dev_chart = alt.Chart(dev_status, title='Devices') \
-                   .mark_bar(tooltip=True) \
-                   .encode(y='status', x='count:Q', row='namespace',
-                           color=alt.Color(
-                               'status',
-                               scale=alt.Scale(domain=['alive', 'dead'],
-                                               range=['green', 'red']))
-                           )
+    if not dev_df.empty:
+        dev_chart = alt.Chart(dev_status, title='Devices') \
+                       .mark_bar(tooltip=True) \
+                       .encode(y='status', x='count:Q', row='namespace',
+                               color=alt.Color(
+                                   'status',
+                                   scale=alt.Scale(domain=['alive', 'dead'],
+                                                   range=['green', 'red']))
+                               )
 
-    if_chart = alt.Chart(if_status, title='Interfaces') \
-                  .mark_bar(tooltip=True) \
-                  .encode(y='state', x='count:Q', row='namespace',
-                          color=alt.Color(
-                              'state',
-                              scale=alt.Scale(domain=['up', 'adminDown',
-                                                      'down'],
-                                              range=['green', 'orange',
-                                                     'red']))
-                          )
+    if not if_df.empty:
+        if_chart = alt.Chart(if_status, title='Interfaces') \
+                      .mark_bar(tooltip=True) \
+                      .encode(y='state', x='count:Q', row='namespace',
+                              color=alt.Color(
+                                  'state',
+                                  scale=alt.Scale(domain=['up', 'adminDown',
+                                                          'down'],
+                                                  range=['green', 'orange',
+                                                         'red']))
+                              )
 
-    bgp_chart = alt.Chart(bgp_status, title='BGP') \
-                   .mark_bar(tooltip=True) \
-                   .encode(y='state', x='count:Q', row='namespace',
-                           color=alt.Color(
-                               'state',
-                               scale=alt.Scale(
-                                   domain=['Established',
-                                           'NotEstd', 'dynamic'],
-                                   range=['green', 'red', 'orange']))
-                           )
+    if not bgp_df.empty:
+        bgp_chart = alt.Chart(bgp_status, title='BGP') \
+                       .mark_bar(tooltip=True) \
+                       .encode(y='state', x='count:Q', row='namespace',
+                               color=alt.Color(
+                                   'state',
+                                   scale=alt.Scale(
+                                       domain=['Established',
+                                               'NotEstd', 'dynamic'],
+                                       range=['green', 'red', 'orange']))
+                               )
 
-    ospf_chart = alt.Chart(ospf_status, title='OSPF') \
-                    .mark_bar(tooltip=True) \
-                    .encode(y='state', x='count:Q', row='namespace',
-                            color=alt.Color(
-                                'state',
-                                scale=alt.Scale(
-                                    domain=['full', 'fail',
-                                            'adminDown', 'passive'],
-                                    range=['green', 'red', 'orange', 'peach']))
-                            )
+    if not ospf_df.empty:
+        ospf_chart = alt.Chart(ospf_status, title='OSPF') \
+                        .mark_bar(tooltip=True) \
+                        .encode(y='state', x='count:Q', row='namespace',
+                                color=alt.Color(
+                                    'state',
+                                    scale=alt.Scale(
+                                        domain=['full', 'fail',
+                                                'adminDown', 'passive'],
+                                        range=['green', 'red', 'orange', 'peach']))
+                                )
 
     with container_1:
         with col1:
-            st.altair_chart(dev_chart)
+            if not dev_df.empty:
+                st.altair_chart(dev_chart)
+            else:
+                st.info('No device info found')
         with col2:
-            st.altair_chart(if_chart)
+            if not if_df.empty:
+                st.altair_chart(if_chart)
+            else:
+                st.info('No interface info found')
 
     container_2 = st.beta_container()
     col2_1, mid, col2_2 = st.beta_columns([2, 1, 2])
     with container_2:
         with col2_1:
-            st.altair_chart(bgp_chart)
+            if not bgp_df.empty:
+                st.altair_chart(bgp_chart)
         with col2_2:
-            st.altair_chart(ospf_chart)
+            if not ospf_df.empty:
+                st.altair_chart(ospf_chart)
