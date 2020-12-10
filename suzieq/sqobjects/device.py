c195291c0851ff3cfdc7a997886381bc43e27895
Dinesh Dutt <dd.ps4u@gmail.com>
2020-12-09 19:35:19 -0800

    device.py: Changes to output due to the GUI
    The GUI display engine messes up uptime column, and so we're going
    back to display only the bootup timestamp. Building the uptime
    column however is necessary for summarize and top verbs.
    
    The changes in this patch are to handle these changes.
    
    Updating tests due to changes caused by corrected time handling
    
    Test updates given the change to device show output change

--- suzieq/sqobjects/device.py
+++ suzieq/sqobjects/device.py
@@ -16,10 +16,10 @@ class DeviceObj(SqObject):
 
         # Convert the bootup timestamp into a time delta
         if 'bootupTimestamp' in df.columns:
-            uptime_cols = (df['timestamp'] -
-                           humanize_timestamp(df['bootupTimestamp']*1000))
+            df['bootupTimestamp'] = humanize_timestamp(
+                df['bootupTimestamp']*1000)
+            uptime_cols = (df['timestamp'] - df['bootupTimestamp'])
             uptime_cols = pd.to_timedelta(uptime_cols, unit='s')
             df.insert(len(df.columns)-1, 'uptime', uptime_cols)
-            df.drop(columns=['bootupTimestamp'], inplace=True)
 
         return df
