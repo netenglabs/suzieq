ff559e3e151dff93574618c31d5f8fcb2a754ec0
Dinesh Dutt <dd.ps4u@gmail.com>
2021-01-10 11:53:16 -0800

    Made the names of the REST certificate files more meaningful

--- suzieq/restServer/sq-rest-server.py
+++ suzieq/restServer/sq-rest-server.py
@@ -15,9 +15,12 @@ def get_cert_files(cfg):
                            os.getenv("HOME") + '/.suzieq/cert.pem')
     ssl_keyfile = cfg.get('rest_keyfile',
                           os.getenv("HOME") + '/.suzieq/key.pem')
-    if not os.path.isfile(ssl_certfile) or not os.path.isfile(ssl_keyfile):
-        print(
-            "ERROR: Missing certificate files, {ssl_certfile}, {ssl_keyfile}")
+    if not os.path.isfile(ssl_certfile):
+        print(f"ERROR: Missing certificate file: {ssl_certfile}")
+        sys.exit(1)
+
+    if not os.path.isfile(ssl_keyfile):
+        print(f"ERROR: Missing certificate file: {ssl_keyfile}")
         sys.exit(1)
 
     return ssl_certfile, ssl_keyfile
@@ -28,7 +31,7 @@ def get_log_file(cfg):
     return f"{tmp}/sq-rest-server.log"
 
 
-def get_log_config():
+def get_log_config(cfg):
     log_config = uvicorn.config.LOGGING_CONFIG
     log_config['handlers']['access']['class'] = 'logging.handlers.RotatingFileHandler'
     log_config['handlers']['access']['maxBytes'] = 10000000
@@ -37,9 +40,9 @@ def get_log_config():
     log_config['handlers']['default']['maxBytes'] = 10_000_000
     log_config['handlers']['default']['backupCount'] = 2
 
-    log_config['handlers']['access']['filename'] = get_log_file()
+    log_config['handlers']['access']['filename'] = get_log_file(cfg)
     del(log_config['handlers']['access']['stream'])
-    log_config['handlers']['default']['filename'] = get_log_file()
+    log_config['handlers']['default']['filename'] = get_log_file(cfg)
     del(log_config['handlers']['default']['stream'])
 
     return log_config
@@ -65,6 +68,6 @@ if __name__ == "__main__":
 
     uvicorn.run(app, host="0.0.0.0", port=8000,
                 log_level=log_level,
-                log_config=get_log_config(),
+                log_config=get_log_config(cfg),
                 ssl_keyfile=ssl_keyfile,
                 ssl_certfile=ssl_certfile)
