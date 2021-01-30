a3839ea64b46e24afff8014d62bb6afe62f40158
Dinesh Dutt <dd.ps4u@gmail.com>
2021-01-30 06:33:12 -0800

    test_sqcmds.py: Made dataframe comparison more rigorous
    Made the dataframe testing more rigorous so that we only catch
    real failures, not failures due to sort order.

--- tests/integration/test_sqcmds.py
+++ tests/integration/test_sqcmds.py
@@ -3,7 +3,6 @@ from suzieq.cli.sqcmds import *
 from nubia import context
 import os
 from tests.conftest import commands, load_up_the_tests, tables
-from tempfile import mkstemp
 import json
 from tests.conftest import setup_sqcmds
 import pandas as pd
@@ -261,12 +260,29 @@ def execute_cmd(cmd, verb, arg, filter=None):
 
 
 def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
-    '''Compare the dataframes for equality'''
+    '''Compare the dataframes for equality
 
-    if ((expected_df.empty and not got_df.empty) or
-            (not expected_df.empty and got_df.empty)):
-        assert(got_df.shape == expected_df.shape)
-        # We assume the asssert failure prevents the code from continuing
+    Comparing data frames is tough. The main thing we're concerned about
+    here is that the contents are identical. We're less concerned about the
+    order. The reason for the lack of comparison of order is because the
+    order could change as a consequence of coalescing or some other change. 
+
+    We work our way from the simplest and fastest attempts to compare equality
+    to slower ways to compare equality. Trying to ignore the sort is the hardest
+    part. First we attempt to sort the two and reset the index to avoid index
+    mismatches. When one of the columns is a list in which case we resort to 
+    deriving tuples of the expected and obtained dataframes, stripping the Index
+    column (should be a range Index only), and then verifying that a row is 
+    present in the other dataframe. We even use sets to attempt a quicker tuple
+    comparison which can again fail due to the presence of a list.
+
+    Real failures are hopefully caught quickly while less clear ones are run
+    through a wringer to verify that there's a real problem.
+    '''
+
+    assert(got_df.shape == expected_df.shape)
+    assert((got_df.columns == expected_df.columns).all())
+    # We assume the asssert failure prevents the code from continuing
 
     if (expected_df.empty and got_df.empty):
         return
@@ -274,19 +290,32 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
     # Drop any columns to be ignored
     if ignore_cols:
         if not got_df.empty:
-            got_df.drop(columns=ignore_cols, inplace=True, errors='ignore')
+            got_df = got_df.drop(columns=ignore_cols, errors='ignore')
         if not expected_df.empty:
-            expected_df.drop(columns=ignore_cols,
-                             inplace=True, errors='ignore')
+            expected_df = expected_df.drop(
+                columns=ignore_cols, errors='ignore')
 
     try:
-        expected_df.sort_values(by=expected_df.columns.tolist(),
-                                inplace=True)
-        got_df.sort_values(by=got_df.columns.tolist(), inplace=True)
+        if isinstance(got_df.index, pd.RangeIndex):
+            expected_df = expected_df \
+                .sort_values(by=expected_df.columns.tolist()) \
+                .reset_index(drop=True)
+        else:
+            expected_df = expected_df \
+                .sort_values(by=expected_df.columns.tolist())
+
+        if isinstance(expected_df.index, pd.RangeIndex):
+            got_df = got_df.sort_values(by=got_df.columns.tolist()) \
+                           .reset_index(drop=True)
+        else:
+            got_df = got_df.sort_values(by=got_df.columns.tolist())
+
     except Exception:
         if 'namespace' in expected_df.columns and 'timestamp' in expected_df.columns:
-            expected_df = expected_df.sort_values(by=['namespace', 'hostname', 'timestamp']).reset_index(drop=True)
-            got_df = got_df.sort_values(by=['namespace', 'hostname', 'timestamp']).reset_index(drop=True)
+            expected_df = expected_df.sort_values(
+                by=['namespace', 'hostname', 'timestamp']).reset_index(drop=True)
+            got_df = got_df.sort_values(
+                by=['namespace', 'hostname', 'timestamp']).reset_index(drop=True)
 
     try:
         rslt_df = expected_df.compare(got_df, keep_equal=True)
@@ -303,6 +332,27 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
                 else:
                     matches = False
                     break
+            if not matches:
+                # This could be because of mismatch in sorted columns
+                # We have assured that the shape is identical already, so try a
+                # manual compare. Skip the index number in this case,
+                # assuming range index, since the sort messes up this order
+                if isinstance(got_df.index, pd.RangeIndex):
+                    got_tuples = [x[1:] for x in got_df.itertuples()]
+                else:
+                    got_tuples = [x for x in got_df.itertuples()]
+                if isinstance(expected_df.index, pd.RangeIndex):
+                    expected_tuples = [x[1:] for x in expected_df.itertuples()]
+                else:
+                    expected_tuples = [x for x in expected_df.itertuples()]
+                try:
+                    assert(set(got_tuples) == set(expected_tuples))
+                except TypeError:
+                    matches = True
+                    for item in expected_tuples:
+                        if item not in got_tuples:
+                            matches = False
+                            assert(rslt_df.empty)
             if not matches:
                 print(rslt_df)
                 assert(rslt_df.empty)
