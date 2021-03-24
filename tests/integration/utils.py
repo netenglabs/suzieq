2a5e32366946f26ed8838cd51be9786e72a310ca
Dinesh Dutt <dd.ps4u@gmail.com>
2021-03-24 15:48:15 -0700

    Print more useful assert messages on failing

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -32,14 +32,14 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
     Comparing data frames is tough. The main thing we're concerned about
     here is that the contents are identical. We're less concerned about the
     order. The reason for the lack of comparison of order is because the
-    order could change as a consequence of coalescing or some other change. 
+    order could change as a consequence of coalescing or some other change.
 
     We work our way from the simplest and fastest attempts to compare equality
     to slower ways to compare equality. Trying to ignore the sort is the hardest
     part. First we attempt to sort the two and reset the index to avoid index
-    mismatches. When one of the columns is a list in which case we resort to 
+    mismatches. When one of the columns is a list in which case we resort to
     deriving tuples of the expected and obtained dataframes, stripping the Index
-    column (should be a range Index only), and then verifying that a row is 
+    column (should be a range Index only), and then verifying that a row is
     present in the other dataframe. We even use sets to attempt a quicker tuple
     comparison which can again fail due to the presence of a list.
 
@@ -47,12 +47,14 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
     through a wringer to verify that there's a real problem.
     '''
 
-    assert(got_df.shape == expected_df.shape)
-    assert((got_df.columns == expected_df.columns).all())
-    # We assume the asssert failure prevents the code from continuing
-
     if (expected_df.empty and got_df.empty):
         return
+
+    elif (not expected_df.empty and got_df.empty):
+        assert False, 'Got unexpected empty dataframe'
+
+    elif (expected_df.empty and not got_df.empty):
+        assert False, 'Got unexpected non-empty dataframe'
 
     # Drop any columns to be ignored
     if ignore_cols:
@@ -80,13 +82,30 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
     except Exception:
         if 'namespace' in expected_df.columns and 'timestamp' in expected_df.columns:
             expected_df = expected_df.sort_values(
-                by=['namespace', 'hostname', 'timestamp']).reset_index(drop=True)
+                by=['namespace', 'hostname']).reset_index(drop=True)
             got_df = got_df.sort_values(
-                by=['namespace', 'hostname', 'timestamp']).reset_index(drop=True)
+                by=['namespace', 'hostname']).reset_index(drop=True)
+
+    if got_df.shape != expected_df.shape:
+        if 'count' in expected_df.columns and (
+                got_df.shape[0] == expected_df.shape[0]):
+            # This is the old unique issue
+            assert got_df.shape == expected_df.shape, 'old unique'
+        else:
+            assert got_df.shape == expected_df.shape, f'{got_df.shape} != {expected_df.shape}'
+    assert (got_df.columns == expected_df.columns).all(
+    ), 'column names/order do not match'
+    # We assume the asssert failure prevents the code from continuing
 
     try:
         rslt_df = expected_df.compare(got_df, keep_equal=True)
         if not rslt_df.empty:
+            # Check if its just the timestamps that are different, as would be the
+            # case if we had a new capture
+            maincols = [x[0] for x in rslt_df.columns.tolist()]
+            if all(x in ['timestamp', 'lastChangeTime', 'bootupTimestamp']
+                   for x in maincols):
+                assert False, 'Only differ in timestamps'
             matches = True
             # If there are lists in the values, their order maybe causing
             # the failure. Pass if the problem is the order but they're
@@ -113,16 +132,17 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
                 else:
                     expected_tuples = [x for x in expected_df.itertuples()]
                 try:
-                    assert(set(got_tuples) == set(expected_tuples))
+                    assert (set(got_tuples) == set(
+                        expected_tuples)), f'{rslt_df}'
                 except TypeError:
                     matches = True
                     for item in expected_tuples:
                         if item not in got_tuples:
                             matches = False
-                            assert(rslt_df.empty)
+                            assert rslt_df.empty, f'{rslt_df}'
             if not matches:
                 print(rslt_df)
-                assert(rslt_df.empty)
+                assert rslt_df.empty, f'{rslt_df}'
     except ValueError:
         # This happens when the two dataframes don't have the same shape
         # such as what happens if the return is an error. So, compare fails
@@ -133,8 +153,8 @@ def assert_df_equal(expected_df, got_df, ignore_cols) -> None:
                                how='outer',
                                indicator=True)
             if not got_df.empty:
-                assert(not rslt_df.empty and rslt_df.query(
-                    '_merge != "both"').empty)
-        except Exception:
+                assert (not rslt_df.empty and rslt_df.query(
+                    '_merge != "both"').empty), 'Merge compare failed'
+        except (Exception, AssertionError, TypeError):
             assert(got_df.shape == expected_df.shape)
             assert('Unable to compare' == '')
