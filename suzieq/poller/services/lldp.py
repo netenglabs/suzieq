cbc2d0001fc15e027e1f538896754f30a26d83d2
Dinesh Dutt <dd.ps4u@gmail.com>
2021-06-16 10:33:03 -0700

    LLDPD Poller: Make data consistent across NOS, fix parsing errors

--- suzieq/poller/services/lldp.py
+++ suzieq/poller/services/lldp.py
@@ -1,46 +1,126 @@
 from suzieq.poller.services.service import Service
 from suzieq.utils import convert_macaddr_format_to_colon
 import re
+import numpy as np
 
 
 class LldpService(Service):
     """LLDP service. Different class because of munging ifname"""
 
-    def get_key_flds(self):
-        """The MAC table in Linux can have weird keys"""
-        return ['ifname', 'peerHostname', 'peerIfname']
-
     def _common_data_cleaner(self, processed_data, raw_data):
-
-        devtype = self._get_devtype_from_input(raw_data)
 
         for i, entry in enumerate(processed_data):
             if not entry:
                 continue
-            if entry.get('_chassisType', '') == 'Mac Address':
-                entry['peerHostname'] = convert_macaddr_format_to_colon(
-                    entry.get('peerHostname', '0000.0000.0000'))
-            if 'peerIfname' in entry:
-                subtype = entry.get('subtype', '')
-                if subtype == 'Mac Address':
-                    entry['subtype'] = 'macddress'
-                    entry['peerMacaddr'] = convert_macaddr_format_to_colon(
-                        entry.get('peerIfname', '0000.0000.0000'))
-                    entry['peerIfname'] = '-'
-                    entry['peerIfindex'] = 0
-                elif subtype == 'ifindex':
-                    entry['subtype'] = 'ifindex'
-                    entry['peerIfname'] = '-'
-                    entry['peerMacaddr'] = '00:00:00:00:00:00'
-                else:
-                    entry['subtype'] = 'ifname'
-                    entry['peerMacaddr'] = '00:00:00:00:00:00'
-                    entry['peerIfindex'] = 0
-
-            if devtype == 'nxos':
-                entry['peerHostname'] = re.sub(r'\(.*\)', '',
-                                               entry['peerHostname'])
-                entry['ifname'] = re.sub(
-                    r'^Eth?(\d)', 'Ethernet\g<1>', entry['ifname'])
+            self._common_cleaner(entry)
 
         return processed_data
+
+    def _common_cleaner(self, entry):
+        if entry.get('_chassisType', '') == 'Mac address':
+            entry['peerHostname'] = convert_macaddr_format_to_colon(
+                entry.get('peerHostname', '0000.0000.0000'))
+
+        entry['subtype'] = entry.get('subtype', '').lower()
+        subtype = entry.get('subtype', '')
+        if subtype == "interface name":
+            entry['peerMacaddr'] = '00:00:00:00:00:00'
+            entry['peerIfindex'] = 0
+        if subtype == 'mac address':
+            entry['peerMacaddr'] = convert_macaddr_format_to_colon(
+                entry.get('peerIfname', '0000.0000.0000'))
+            entry['peerIfname'] = '-'
+            entry['peerIfindex'] = 0
+        elif subtype.startswith('locally'):
+            entry['peerIfindex'] = entry['peerIfname']
+            entry['peerIfname'] = '-'
+            entry['peerMacaddr'] = '00:00:00:00:00:00'
+
+    def _clean_nxos_data(self, processed_data, raw_data):
+
+        drop_indices = []
+        entries = {}
+
+        for i, entry in enumerate(processed_data):
+            entry['peerHostname'] = re.sub(r'\(.*\)', '',
+                                           entry['peerHostname'])
+            entry['ifname'] = re.sub(
+                r'^Eth?(\d)', 'Ethernet\g<1>', entry['ifname'])
+
+            if entry['ifname'] in entries:
+                # Description is sometimes filled in with CDP, but not LLDP
+                if not entry.get('description', ''):
+                    old_entry = processed_data[entries[entry['ifname']]]
+                    entry['description'] = old_entry.get('description', '')
+                    drop_indices.append(entries[entry['ifname']])
+            else:
+                entries[entry['ifname']] = i
+
+            if entry.get('protocol', '') == 'cdp':
+                entry['subtype'] = 'interface name'
+
+            self._common_cleaner(entry)
+            if not entry['peerHostname']:
+                drop_indices.append(i)
+
+        processed_data = np.delete(processed_data, drop_indices).tolist()
+        return processed_data
+
+    def _clean_junos_data(self, processed_data, raw_data):
+        for entry in processed_data:
+            self._common_cleaner(entry)
+        return processed_data
+
+    def _clean_eos_data(self, processed_data, raw_data):
+
+        drop_indices = []
+
+        for i, entry in enumerate(processed_data):
+            subtype = entry.get('subtype', '')
+            if subtype == 'interfaceName':
+                entry['subtype'] = 'interface name'
+            elif subtype == 'macAddress':
+                entry['subtype'] = 'mac address'
+            elif subtype == "local":
+                entry['subtype'] = 'locally assigned'
+
+            self._common_cleaner(entry)
+            if not entry['peerHostname']:
+                drop_indices.append(i)
+
+        processed_data = np.delete(processed_data, drop_indices).tolist()
+        return processed_data
+
+    def _clean_cumulus_data(self, processed_data, raw_data):
+        for entry in processed_data:
+            subtype = entry.get('subtype', '')
+            if subtype == "ifname":
+                entry['subtype'] = "interface name"
+            elif subtype == "local":
+                entry['subtype'] = "locally assigned"
+
+            self._common_cleaner(entry)
+
+        return processed_data
+
+    def _clean_linux_data(self, processed_data, raw_data):
+        for entry in processed_data:
+            subtype = entry.get('subtype', '')
+            if subtype == 'ifname':
+                entry['subtype'] = 'interface name'
+            elif subtype == 'local':
+                entry['subtype'] = 'locally assigned'
+            elif subtype == 'mac':
+                entry['subtype'] = 'mac address'
+            self._common_cleaner(entry)
+
+        return processed_data
+
+    def _clean_iosxr_data(self, processed_data, raw_data):
+        for entry in processed_data:
+            self._common_cleaner(entry)
+
+        return processed_data
+
+    def _clean_sonic_data(self, processed_data, raw_data):
+        return self._clean_linux_data(processed_data, raw_data)
