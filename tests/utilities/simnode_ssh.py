49bfdc540cccc97cd3c992c51bd49bf0223ff276
Dinesh Dutt <dd.ps4u@gmail.com>
2021-07-18 18:14:51 -0700

    Simulated IOS XE node support
    Since I don't have an IOS XE node, added a simulation via an SSH server
    that serves out pre-canned outputs to IOSXE commands.

--- tests/utilities/simnode_ssh.py
+++ tests/utilities/simnode_ssh.py
@@ -7,22 +7,14 @@ from importlib.util import find_spec
 
 
 class MySSHServerSession(asyncssh.SSHServerSession):
-    def __init__(self):
+    def __init__(self, device='iosxr'):
         self._input = ''
         self._data = None
-        self.devtype = 'iosxr'
+        self.device = device
         self.run_as_shell = False
-        self.prompt = '#'
+        self.prompt = '# '
         self.vendor = 'cisco'
-        self.sample_data_dir = self.get_testinput_dir()
-        self.cmd_data = {
-            'show version':
-            f'{self.sample_data_dir}/cisco/iosxr/show_version.txt',
-            'show run hostname':
-            f'{self.sample_data_dir}/cisco/iosxr/show_run_hostname.txt',
-            'show interfaces':
-            f'{self.sample_data_dir}/cisco/iosxr/show_interfaces.txt',
-        }
+        self.sample_data_dir = './tests/integration/nossim/'
 
     def get_testinput_dir(self):
         '''Get the dir where the test input data is stored'''
@@ -36,14 +28,50 @@ class MySSHServerSession(asyncssh.SSHServerSession):
         self.run_as_shell = True
         return True
 
+    def eof_received(self):
+        return False
+
+    def get_cmd_file(self, command: str, fmt: str = '.txt') -> str:
+
+        self.cmd_data = {
+            'show version':
+            f'{self.sample_data_dir}/{self.device}/show_version{fmt}',
+            'show run hostname':
+            f'{self.sample_data_dir}/{self.device}/show_run_hostname{fmt}',
+            'show interfaces':
+            f'{self.sample_data_dir}/{self.device}/show_interfaces{fmt}',
+            'show ethernet-switching table detail':
+            f'{self.sample_data_dir}/{self.device}/show_ethernet_switching_table{fmt}',
+            'show system uptime':
+            f'{self.sample_data_dir}/{self.device}/show_system_uptime{fmt}',
+            'show mac-address table':
+            f'{self.sample_data_dir}/{self.device}/show_mac_address_table{fmt}',
+            'show ip arp':
+            f'{self.sample_data_dir}/{self.device}/show_ip_arp{fmt}',
+            'show ipv6 neighbors':
+            f'{self.sample_data_dir}/{self.device}/show_ipv6_neighbors{fmt}',
+            'show ip route vrf *':
+            f'{self.sample_data_dir}/{self.device}/show_ip_route{fmt}',
+            'show ipv6 route vrf *':
+            f'{self.sample_data_dir}/{self.device}/show_ipv6_route{fmt}',
+        }
+
+        return self.cmd_data.get(command, '')
+
     def _exec_cmd(self, command):
         '''The routine to execute command and return data'''
         data = 'Command not found\n'
         if command in ['exit', 'quit']:
             self.eof_received()
             return ''
-        if command in self.cmd_data:
-            with open(self.cmd_data[command], 'r') as f:
+        if 'json' in command:
+            command = command.split('|')[0].strip()
+            fmt = '.json'
+        else:
+            fmt = '.txt'
+        cmdfile = self.get_cmd_file(command, fmt)
+        if cmdfile:
+            with open(cmdfile, 'r') as f:
                 data = f.read()
 
         return data
@@ -75,8 +103,9 @@ class MySSHServerSession(asyncssh.SSHServerSession):
 
 
 class MySSHServer(asyncssh.SSHServer):
-    def __init__(self):
+    def __init__(self, device='iosxr'):
         self.passwords = {'vagrant': 'vaqRzE48Dulhs'}   # password of 'vagrant'
+        self.device = device
 
     def connection_made(self, conn):
         print('SSH connection received from %s.' %
@@ -100,18 +129,51 @@ class MySSHServer(asyncssh.SSHServer):
         return crypt.crypt(password, pw) == pw
 
     def session_requested(self):
-        return MySSHServerSession()
+        return MySSHServerSession(device=self.device)
 
 
-async def start_server():
-    await asyncssh.listen('', 10000, server_factory=MySSHServer,
-                          server_host_keys=['/tmp/ssh_host_key'])
+class IOSXRServer(MySSHServer):
+    def __init__(self):
+        super().__init__(device='iosxr')
 
-loop = asyncio.get_event_loop()
 
-try:
-    loop.run_until_complete(start_server())
-except (OSError, asyncssh.Error) as exc:
-    sys.exit('Error starting server: ' + str(exc))
+class IOSXEServer(MySSHServer):
+    def __init__(self):
+        super().__init__(device='iosxe')
 
-loop.run_forever()
+
+class NXOSServer(MySSHServer):
+    def __init__(self):
+        super().__init__(device='nxos')
+
+
+class EOSServer(MySSHServer):
+    def __init__(self):
+        super().__init__(device='eos')
+
+
+class QFXServer(MySSHServer):
+    def __init__(self):
+        super().__init__(device='qfx')
+
+
+async def start_server(device='iosxr'):
+    factory = {
+        'iosxr': IOSXRServer,
+        'nxos': NXOSServer,
+        'eos': EOSServer,
+        'iosxe': IOSXEServer,
+        'qfx': QFXServer,
+    }
+    await asyncssh.listen('', 10000, server_factory=factory[device],
+                          server_host_keys=['/home/ddutt/work/suzieq/play/ssh_host_key'])
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+
+    try:
+        loop.run_until_complete(start_server(sys.argv[1]))
+    except (OSError, asyncssh.Error) as exc:
+        sys.exit('Error starting server: ' + str(exc))
+
+    loop.run_forever()
