f1ee7595583419cf42d54517fc879309127ba279
Dinesh Dutt <dd.ps4u@gmail.com>
2021-08-07 09:35:16 -0700

    More automated, efficient docker build

---
+++ build-docker.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+cd $PYTHONPATH
+VERSION=`./suzieq/version.py`
+poetry build
+if [ $# -eq 0 ]; then
+    BUILD_TAG=$VERSION
+else
+    BUILD_TAG=$VERSION-$1
+fi
+poetry export -f requirements.txt --without-hashes > /tmp/requirements.txt
+diff -q build/requirements.txt /tmp/requirements.txt > /dev/null
+if [ $? -ne 0 ]; then
+    echo "Building image suzieq-base:$VERSION"
+    poetry export -f requirements.txt --without-hashes > ./build/requirements.txt
+    docker build -t suzieq-base:$VERSION -f Dockerfile-sqbase .
+fi
+BASE_EXISTS=`docker images -q suzieq-base:$VERSION`
+if [ -z "$BASE_EXISTS" ]; then
+    echo "Building image suzieq-base:$VERSION"
+    docker build -t suzieq-base:$VERSION -f Dockerfile-sqbase .
+fi
+echo "Building image ddutt/suzieq:$BUILD_TAG"
+docker build --build-arg version=$VERSION -t ddutt/suzieq:$BUILD_TAG -t netenglabs/suzieq:$BUILD_TAG .
