d5c0bc25043a18120c2fa93fb2ff6203bdf23275
Dinesh Dutt <dd.ps4u@gmail.com>
2021-08-12 21:05:19 -0700

    Fix #403: Fix the REST API server startup inside docker

--- suzieq/restServer/sq_rest_server.py
+++ suzieq/restServer/sq_rest_server.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
 
 import sys
-import argparse
-
 from suzieq.restServer.query import rest_main
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-c",
-        "--config",
-        type=str, help="alternate config file",
-        default=None
-    )
-    parser.add_argument(
-        "--no-https",
-        help="Turn off HTTPS",
-        default=False, action='store_true',
-    )
-    userargs = parser.parse_args()
-    rest_main(userargs.config, userargs.no_https)
+    rest_main(sys.argv)
