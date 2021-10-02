c082853416fe2cbbd32b566f7de9012d43536495
Dinesh Dutt <dd.ps4u@gmail.com>
2021-10-02 11:39:20 -0700

    Make Path from the CLI perspective to be the same as any other table w.r.t verbs

--- suzieq/cli/sqcmds/PathCmd.py
+++ suzieq/cli/sqcmds/PathCmd.py
@@ -8,6 +8,11 @@ from suzieq.sqobjects.path import PathObj
 
 @command("path", help="build and act on path data")
 class PathCmd(SqCommand):
+    """Path trace information including overlay and underlay"""
+
+    @argument("src", description="Source IP address, in quotes")
+    @argument("dest", description="Destination IP address, in quotes")
+    @argument("vrf", description="VRF to trace path in")
     def __init__(
         self,
         engine: str = "",
@@ -18,6 +23,9 @@ class PathCmd(SqCommand):
         namespace: str = "",
         format: str = "",
         columns: str = "default",
+        src: str = "",
+        dest: str = "",
+        vrf: str = "",
     ) -> None:
         super().__init__(
             engine=engine,
@@ -30,12 +38,12 @@ class PathCmd(SqCommand):
             format=format,
             sqobj=PathObj
         )
+        self.src = src
+        self.dest = dest
+        self.vrf = vrf
 
     @command("show")
-    @argument("src", description="Source IP address, in quotes")
-    @argument("dest", description="Destination IP address, in quotes")
-    @argument("vrf", description="VRF to trace path in")
-    def show(self, src: str = "", dest: str = "", vrf: str = ''):
+    def show(self):
         """show paths between specified from source to target ip addresses"""
         # Get the default display field names
         if self.columns is None:
@@ -50,8 +58,8 @@ class PathCmd(SqCommand):
         try:
             df = self.sqobj.get(
                 hostname=self.hostname, columns=self.columns,
-                namespace=self.namespace, source=src, dest=dest,
-                vrf=vrf
+                namespace=self.namespace, source=self.src, dest=self.dest,
+                vrf=self.vrf
             )
         except Exception as e:
             if len(e.args) > 1:
@@ -66,10 +74,7 @@ class PathCmd(SqCommand):
             return self._gen_output(df, sort=False)
 
     @command("summarize")
-    @argument("src", description="Source IP address, in quotes")
-    @argument("dest", description="Destination IP address, in quotes")
-    @argument("vrf", description="VRF to trace path in")
-    def summarize(self, src: str = "", dest: str = "", vrf: str = ''):
+    def summarize(self):
         """Summarize paths between specified from source to target ip addresses"""
         # Get the default display field names
         if self.columns is None:
@@ -84,7 +89,7 @@ class PathCmd(SqCommand):
         try:
             df = self.sqobj.summarize(
                 hostname=self.hostname, namespace=self.namespace,
-                source=src, dest=dest, vrf=vrf
+                source=self.src, dest=self.dest, vrf=self.vrf
             )
         except Exception as e:
             df = pd.DataFrame({'error': ['ERROR: {}'.format(str(e))]})
@@ -93,9 +98,66 @@ class PathCmd(SqCommand):
         if not df.empty:
             return self._gen_output(df)
 
-    @command("unique", help="find the list of unique items in a column")
-    def unique(self, **kwargs):
+    @command("unique")
+    @argument("count", description="include count of times a value is seen",
+              choices=['True'])
+    def unique(self, count: str = 'False'):
+        """Display unique values for specified field of a path"""
+        # Get the default display field names
+        if self.columns is None:
+            return
 
-        msg = 'ERROR: Unique not supported for this object'
-        df = pd.DataFrame({'error': [msg]})
-        return self._gen_output(df, dont_strip_cols=True)
+        now = time.time()
+        if self.columns != ["default"]:
+            self.ctxt.sort_fields = None
+        else:
+            self.ctxt.sort_fields = []
+
+        try:
+            df = self.sqobj.unique(
+                hostname=self.hostname, namespace=self.namespace,
+                source=self.src, dest=self.dest, vrf=self.vrf,
+                count=count
+            )
+        except Exception as e:
+            df = pd.DataFrame({'error': ['ERROR: {}'.format(str(e))]})
+
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+        if not df.empty:
+            return self._gen_output(df)
+
+    @command("top", help="find the top n values for a field")
+    @argument("count", description="number of rows to return")
+    @argument("what", description="integer field to get top values for")
+    @argument("reverse", description="return bottom n values",
+              choices=['True', 'False'])
+    def top(self, count: int = 5, what: str = '', reverse: str = 'False',
+            **kwargs) -> int:
+        """Return the top n values for a field in path trace output
+
+        Args:
+            count (int, optional): The number of entries to return. Defaults to 5
+            what (str, optional): Field name to use for largest/smallest val
+            reverse (bool, optional): Reverse and return n smallest
+
+        Returns:
+            int: 0 or error code
+        """
+        now = time.time()
+
+        df = self.sqobj.top(hostname=self.hostname,
+                            namespace=self.namespace,
+                            what=what, count=count, reverse=eval(reverse),
+                            source=self.src, dest=self.dest, vrf=self.vrf,
+                            )
+
+        self.ctxt.exec_time = "{:5.4f}s".format(time.time() - now)
+        if 'error' in df.columns:
+            return self._gen_output(df)
+
+        if not df.empty:
+            df = self.sqobj.humanize_fields(df)
+            return self._gen_output(df.sort_values(by=[what], ascending=False),
+                                    dont_strip_cols=True, sort=False)
+        else:
+            return self._gen_output(df)
