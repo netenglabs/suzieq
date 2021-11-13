16d6016228108d6647d2086b38401f373a2a98db
Dinesh Dutt <dd.ps4u@gmail.com>
2021-11-13 09:16:28 -0800

    sqpoller: Bug fix to build query string correctly in all cases

--- suzieq/engines/pandas/sqPoller.py
+++ suzieq/engines/pandas/sqPoller.py
@@ -11,6 +11,9 @@ class SqpollerObj(SqPandasEngine):
 
         status = kwargs.pop('status', '')
         poll_period_exceeded = kwargs.pop('pollExcdPeriodCount', '')
+        columns = kwargs.pop('columns', [])
+
+        drop_cols = []
 
         if status == "pass":
             add_filter = 'status == 0 or status == 200'
@@ -23,16 +26,31 @@ class SqpollerObj(SqPandasEngine):
         # pick the last entry which was not 0, for example, even if it
         # is not the latest, which is incorrect. Just like status=down
         if add_filter:
-            add_filter += ' and '
+            add_prefix = ' and '
+        else:
+            add_prefix = ''
+
         if poll_period_exceeded:
             if poll_period_exceeded.startswith('!'):
-                add_filter += 'pollExcdPeriodCount != 0'
+                add_filter += f'{add_prefix}pollExcdPeriodCount != 0'
             else:
-                add_filter += 'pollExcdPeriodCount == 0'
+                add_filter += f'{add_prefix}pollExcdPeriodCount == 0'
+
+        fields = self.schema.get_display_fields(columns)
+        if status and 'status' not in fields:
+            fields.append('status')
+            drop_cols.append('status')
+
+        if poll_period_exceeded and 'pollExcdPeriodCount' not in fields:
+            fields.append('pollExcdPeriodCount')
+            drop_cols.append('pollExcdPeriodCount')
 
         df = super().get(add_filter=add_filter, **kwargs)
         if not df.empty and add_filter:
-            return df.query(add_filter).reset_index(drop=True)
+            df = df.query(add_filter).reset_index(drop=True)
+
+        if drop_cols:
+            df = df.drop(drop_cols, axis=1, errors='ignore')
 
         return df
 
