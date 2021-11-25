cea4a941bca24f320b597a61fa7f8c92946f54fe
Luca Nicosia <lucassa5@live.com>
2021-11-25 11:54:03 +0100

    Added token generation to netbox test
    Signed-off-by: Luca Nicosia <lucassa5@live.com>

--- suzieq/inventoryProvider/tests/netbox_rest_server.py
+++ suzieq/inventoryProvider/tests/netbox_rest_server.py
@@ -1,17 +1,19 @@
 import uvicorn
 from typing import Optional
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Request
 from os.path import isfile, abspath
 import json
 
 FILE_NAME = "data.json"
 SUZIEQ_PATH = abspath("./suzieq/inventoryProvider/tests/")
-ERRORS_PATH = SUZIEQ_PATH + "errors/"
+ERRORS_PATH = SUZIEQ_PATH + "/errors/"
 
 app = FastAPI()
 
+
 def rest_main():
-    uvicorn.run(app, host='', port=9000)
+    uvicorn.run(app, host="", port=9000)
+
 
 def getData(path):
     file_path = SUZIEQ_PATH + path + FILE_NAME
@@ -20,19 +22,24 @@ def getData(path):
     with open(file_path, "r") as f:
         return json.loads(f.read()), None
 
+
 def getError(error):
     error_path = ERRORS_PATH + error
     if not isfile(error_path):
-        print(error_path)
         raise ValueError("Unknown error {}".format(error))
     with open(error_path, "r") as f:
-        return f.read()
+        error = f.read()
+        try:
+            return json.loads(error)
+        except Exception:
+            return error
+
 
 @app.get("/api/dcim/devices/")
 async def getDevices(
     tag: Optional[str] = Query(None, max_length=50),
     limit: Optional[int] = Query(None),
-    offset: Optional[int] = Query(None)
+    offset: Optional[int] = Query(None),
 ):
     result, error = getData("/api/dcim/devices/")
     if error:
@@ -40,7 +47,9 @@ async def getDevices(
     in_devices = result.get("results", [])
     devices = []
     for d in in_devices:
-        if len(list(filter(lambda t: t["name"] == tag, d.get("tags", [])))) > 0:
+        if len(
+            list(filter(lambda t: t["name"] == tag, d.get("tags", [])))
+        ) > 0:
             devices.append(d)
 
     if not devices:
@@ -67,22 +76,34 @@ async def getDevices(
         devices = devices[offset:]
         if offset - limit < 0:
             res["previous"] = "/api/dcim/devices/?limit={}&tag={}"\
-                          .format(limit, tag)
+                              .format(limit, tag)
         else:
             res["previous"] = "/api/dcim/devices/?limit={}&offset={}&tag={}"\
-                          .format(limit, offset - limit, tag)
+                              .format(limit, offset - limit, tag)
 
     if len(devices) > limit:
         devices = devices[:limit]
-        res["next"] = "/api/dcim/devices/?limit={}&offset={}&tag={}"\
-                      .format(limit, offset + limit, tag)
+        res["next"] = "/api/dcim/devices/?limit={}&offset={}&tag={}".format(
+            limit, offset + limit, tag
+        )
 
     res["results"] = devices
     return res
 
+
 @app.post("/api/users/tokens/provision/")
-async def getToken():
-    return {"token":"token-value"}
+async def getToken(request: Request):
+    valid_credentials = {"username": "admin", "password": "admin"}
+    credentials = await request.json()
+    if credentials != valid_credentials:
+        return getError("invalid_credentials")
+
+    result, error = getData("/api/users/tokens/provision/")
+    if error:
+        return error
+
+    return result
+
 
 if __name__ == "__main__":
     rest_main()
