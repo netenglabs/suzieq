fcfc627a2b0edddf79f77fff2911d12d0c49a79c
Luca Nicosia <lucassa5@live.com>
2021-11-30 13:44:25 +0100

    a bit of linting
    Signed-off-by: Luca Nicosia <lucassa5@live.com>

--- suzieq/inventoryProvider/plugins/basePlugins/chunker.py
+++ suzieq/inventoryProvider/plugins/basePlugins/chunker.py
@@ -1,12 +1,22 @@
+"""This module contains the base class for a Chunker.
+
+Classes:
+    Chunker: The duty of a chunker is to get the global inventory and split it
+             in smaller chunks
+"""
 from abc import abstractmethod
 
+
 class Chunker:
+    """Abstract class for a Chunker
+    """
     @abstractmethod
     def chunk(self, glob_inv, n_pollers, **addl_params):
-        ''' 
-        Split the global inventory in <n_chunks> chunks 
-        
-        addl_parameters: this field could be used to specify additional parameters
-                         to say how to split the global inventory
-        '''
-        pass
+        """Split the global inventory in <n_pollers> chunks
+
+        Args:
+            glob_inv ([type]): global inventory to split
+            n_pollers ([type]): number of chunks
+            addl_parameters ([type]): custom parameters that each Chunker
+                                      can define
+        """
--- suzieq/inventoryProvider/sq_inventory_provider.py
+++ suzieq/inventoryProvider/sq_inventory_provider.py
@@ -1,9 +1,23 @@
+"""This module manages and coordinates all the plugins
+
+Classes:
+    InventoryProvider: manages all the plugins
+
+Functions:
+    sq_main(): this function coordinates all the plugins initializing
+                and starting each one of them. The plugins are divided
+                in types. The supported types are:
+                - basePlugins.InventorySource
+                - basePlugins.Chunker
+                - basePlugins.PollerManager
+"""
+from os.path import join, isfile
+from typing import Type, List
+import threading
+import argparse
 import yaml
 from suzieq.inventoryProvider.utils \
     import INVENTORY_PROVIDER_PATH, get_class_by_path, get_classname_from_type
-from os.path import join
-import threading
-import argparse
 
 
 PLUGIN_PATH = join(INVENTORY_PROVIDER_PATH, "plugins")
@@ -11,6 +25,8 @@ BASE_PLUGIN_PATH = join(PLUGIN_PATH, "basePlugins")
 
 
 class InventoryProvider:
+    """This class manages all the plugins set on the configuration files
+    """
     def __init__(self) -> None:
         self._provider_config = dict()
 
@@ -21,14 +37,38 @@ class InventoryProvider:
         # contains the Plugin objects divided by type
         self._plugin_objects = dict()
 
-    def load(self, config_data):
+    def load(self, config_data: dict):
+        """Loads the provider configuration and the plugins configurations
+
+        Args:
+            config_data ([dict]): a dictionary which contains provider and
+                                  plugins configurations
+        """
         self._provider_config = config_data.get("provider_config", {})
         self._plugins_config = config_data.get("plugin_type", {})
 
-    def get_plugins(self, plugin_type):
+    def get_plugins(self, plugin_type: str) -> List[Type]:
+        """Returns the list of plugins of type <plugin_type>
+
+        Args:
+            plugin_type (str): type of the plugins to be returned
+
+        Returns:
+            List[Type]: list of plugins of type <plugin_type>
+        """
         return self._plugin_objects.get(plugin_type, None)
 
-    def init_plugins(self, plugin_type):
+    def init_plugins(self, plugin_type: str):
+        """Initialize all plugins of type <plugin_type>
+
+        Args:
+            plugin_type (str): type of plugins to initialize
+
+        Raises:
+            RuntimeError: No plugin configuration
+            RuntimeError: Invalid configuration
+            RuntimeError: Unknown plugin
+        """
         plugin_confs = self._plugins_config.get(plugin_type, {})
         if not plugin_confs:
             raise RuntimeError("No plugin configuration provided")
@@ -41,24 +81,31 @@ class InventoryProvider:
             ppath, BASE_PLUGIN_PATH, base_class_name
         )
 
-        for pc in plugin_confs:
-            pname = pc.get("plugin_name", "")
+        for plug_conf in plugin_confs:
+            pname = plug_conf.get("plugin_name", "")
             if not pname:
                 raise RuntimeError("Missing field <plugin_name>")
             if pname not in plugin_classes:
                 raise RuntimeError(
-                    "Unknown plugin called {} with type {}"
-                    .format(pname, plugin_type)
+                    f"Unknown plugin called {pname} with type {plugin_type}"
                 )
 
-            plugin = plugin_classes[pname](pc.get("args", {}))
+            plugin = plugin_classes[pname](plug_conf.get("args", {}))
             if not self._plugin_objects.get(plugin_type, None):
                 self._plugin_objects[plugin_type] = []
             self._plugin_objects[plugin_type].append(plugin)
 
 
 def sq_prov_main():
+    """InventoryProvider main function
 
+    This function loads all the plugins provided in the configuration file and
+    coordinates all the different plugins
+
+    Raises:
+        RuntimeError: Cannot find the configuration file
+        RuntimeError: Missing inventorySource plugins in the configuration
+    """
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -81,35 +128,39 @@ def sq_prov_main():
     run_once = args.run_once
 
     if config_file:
-
+        if not isfile(config_file):
+            raise RuntimeError(f"No configuration file at {config_file}")
         config_data = {}
         with open(
             config_file,
             "r",
-        ) as fp:
-            file_data = fp.read()
-            config_data = yaml.safe_load(file_data)
+            encoding="utf-8"
+        ) as file:
+            config_data = yaml.safe_load(file.read())
 
-        ni = InventoryProvider()
-        ni.load(config_data.get("inventory_provider", {}))
+        inv_prov = InventoryProvider()
+        inv_prov.load(config_data.get("inventory_provider", {}))
 
-        ni.init_plugins("inventorySource")
+        inv_prov.init_plugins("inventorySource")
 
-        invSrc_plugins = ni.get_plugins("inventorySource")
-        if not invSrc_plugins:
+        inv_source_plugins = inv_prov.get_plugins("inventorySource")
+        if not inv_source_plugins:
             raise RuntimeError(
                 "No inventorySource plugin in the configuration file"
             )
 
-        invSrc_threads = list()
+        inv_source_threads = list()
 
-        for p in invSrc_plugins:
-            t = threading.Thread(target=p.run, kwargs={"run_once": run_once})
-            invSrc_threads.append(t)
-            t.start()
+        for is_plugin in inv_source_plugins:
+            thead = threading.Thread(
+                target=is_plugin.run,
+                kwargs={"run_once": run_once}
+            )
+            inv_source_threads.append(thead)
+            thead.start()
 
-        for p in invSrc_plugins:
-            print(p.get_inventory())
+        for is_plugin in inv_source_plugins:
+            print(is_plugin.get_inventory())
 
 
 if __name__ == "__main__":
