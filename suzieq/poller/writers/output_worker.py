34b85f2d19ccf9cf2b6766d4277e1b5be2c84574
Claudio <claudio.lorina@gmail.com>
2021-12-08 17:57:14 +0100

    Fix error check in OutputWorker base class
    Signed-off-by: Claudio <claudio.lorina@gmail.com>

--- suzieq/poller/writers/output_worker.py
+++ suzieq/poller/writers/output_worker.py
@@ -6,6 +6,7 @@ import abc
 import logging
 import os
 from typing import Dict
+from suzieq.shared.exceptions import SqPollerConfError
 
 from suzieq.shared.sq_plugin import SqPlugin
 
@@ -15,19 +16,21 @@ class OutputWorker(SqPlugin):
     implementing the logic to persist the output of the polling
     """
     def __init__(self, **kwargs):
-        self.type = kwargs.get("type", None)
+        self.type = kwargs.get('type', None)
         self.logger = logging.getLogger(__name__)
 
-        output_dir = kwargs.get("output_dir", None)
+        output_dir = kwargs.get('output_dir', None)
         if output_dir:
             self.root_output_dir = output_dir
-            if not os.path.isdir(output_dir):
+            if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
+            elif not os.path.isdir(output_dir):
+                raise SqPollerConfError(f'Output directory {output_dir}'
+                                        'is not a directory')
         else:
             # TBD: The right error to raise here since this is a required
             # keyword
-            self.logger.error("Need mandatory keyword arg: output_dir")
-            raise ValueError
+            raise SqPollerConfError('Need mandatory keyword arg: output_dir')
 
     @abc.abstractmethod
     def write_data(self, data: Dict):
