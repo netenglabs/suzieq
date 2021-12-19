3b4d1415b398843ecef6791802f4b2daa9ec3909
Dinesh Dutt <dd.ps4u@gmail.com>
2021-12-18 23:49:48 -0800

    evpnVni: Simplified command model, support all filters on all verbs
    Signed-off-by: Dinesh Dutt <dd.ps4u@gmail.com>

--- suzieq/sqobjects/evpnVni.py
+++ suzieq/sqobjects/evpnVni.py
@@ -8,7 +8,7 @@ class EvpnvniObj(SqObject):
         super().__init__(table='evpnVni', **kwargs)
         self._valid_get_args = ['namespace', 'hostname', 'columns', 'vni',
                                 'priVtepIp', 'query_str']
-        self._valid_assert_args = ['namespace', 'hostname', 'status']
+        self._valid_assert_args = self._valid_get_args + ['status']
         self._valid_arg_vals = {
             'status': ['all', 'pass', 'fail'],
         }
