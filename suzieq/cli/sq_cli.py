a60d293e22ae2a28f90e211ec6ed1140edf15d00
Dinesh Dutt <dd.ps4u@gmail.com>
2021-12-18 22:32:29 -0800

    Collect commands the simplified way nubia folks recommend
    Signed-off-by: Dinesh Dutt <dd.ps4u@gmail.com>

--- suzieq/cli/sq_cli.py
+++ suzieq/cli/sq_cli.py
@@ -3,13 +3,16 @@
 import sys
 from nubia import Nubia, Options
 from suzieq.cli.sq_nubia_plugin import NubiaSuzieqPlugin
+from suzieq.cli import sqcmds
 
 
 def cli_main():
     '''Kicks off the CLI run'''
+
     plugin = NubiaSuzieqPlugin()
     shell = Nubia(name="suzieq", plugin=plugin,
-                  options=Options(persistent_history=True))
+                  options=Options(persistent_history=True),
+                  command_pkgs=sqcmds)
     sys.exit(shell.run())
 
 
