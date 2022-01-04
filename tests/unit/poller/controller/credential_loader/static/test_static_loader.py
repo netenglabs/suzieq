926c5d622e7702ced766c948f10ea098054d4341
LucaNicosia <lucassa5@live.com>
2022-01-04 12:11:24 +0100

    Adding tests for static credential loader
    Signed-off-by: LucaNicosia <lucassa5@live.com>

---
+++ tests/unit/poller/controller/credential_loader/static/test_static_loader.py
@@ -0,0 +1,47 @@
+from typing import Dict
+import pytest
+from suzieq.poller.controller.credential_loader.static import StaticLoader
+from tests.unit.poller.controller.utils import read_data
+
+_DATA_PATH = [
+    {
+        'inventory': 'tests/unit/poller/controller/credential_loader/static'
+        '/data/inventory.yaml',
+        'results': 'tests/unit/poller/controller/credential_loader/static'
+        '/data/results.yaml'
+    }
+]
+
+
+@pytest.mark.poller
+@pytest.mark.controller
+@pytest.mark.credential_loader
+@pytest.mark.static_credential_loader
+@pytest.mark.parametrize('data_path', _DATA_PATH)
+def test_load(data_path: Dict):
+    """Test if the credentials are loaded correctly
+
+    The info loaded from the configuration must be override
+    the one inside a node only if it was not specified
+
+    Args:
+        data_path (Dict): input dictionary containing the inventory
+        and the results
+    """
+    # the username will be overrided by the content of the inventory
+    init_data = {
+        'username': 'user-to-override',
+        'ssh-passphrase': 'plain:my-pass',
+        'password': 'plain:password'
+    }
+
+    sl = StaticLoader(init_data)
+
+    assert sl._conf_username == init_data['username']
+    assert sl._conf_passphrase == init_data['ssh-passphrase'].split(':')[1]
+    assert sl._conf_password == init_data['password'].split(':')[1]
+
+    inv = read_data(data_path['inventory'])
+    sl.load(inv)
+
+    assert inv == read_data(data_path['results'])
