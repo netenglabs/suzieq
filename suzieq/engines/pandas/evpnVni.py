cf77ab658b58f12116d0cff1050fb1445177f894
Dinesh Dutt <dd.ps4u@gmail.com>
2022-01-30 06:52:24 -0800

    Improve EVPN VNI assert
    EVPN VNI assert was slow because it attempted to verify reachability to
    every remote VTEP known to a VTEP. This was done because once upon a
    time one of the implementations didn't handle checking this reachability
    before installing the routes.
    
    We also enabled the check needed on Linux/FRR-based NOS (Cumulus &
    SONiC) to check that the mac table has all the appropriate entries for
    doing ingress replication.
    
    Also for systems such as Junos that don't have a VXLAN interface, do not
    do any Vxlan interface checks.
    
    Signed-off-by: Dinesh Dutt <dd.ps4u@gmail.com>

--- suzieq/engines/pandas/evpnVni.py
+++ suzieq/engines/pandas/evpnVni.py
@@ -205,16 +205,16 @@ class EvpnvniObj(SqPandasEngine):
         else:
             her_df = pd.DataFrame()
 
-        if (not her_df.empty and
-                (her_df.remoteVtepList.str.len() != 0).any()):
-            # Check if every VTEP we know is reachable
-            rdf = self._get_table_sqobj('routes').get(
-                namespace=kwargs.get('namespace'), vrf='default')
-            if not rdf.empty:
-                rdf['prefixlen'] = rdf['prefix'].str.split('/') \
-                                                    .str[1].astype('int')
-            her_df["assertReason"] += her_df.apply(
-                self._is_vtep_reachable, args=(rdf,), axis=1)
+        # if (not her_df.empty and
+        #         (her_df.remoteVtepList.str.len() != 0).any()):
+        #     # Check if every VTEP we know is reachable
+        #     rdf = self._get_table_sqobj('routes').get(
+        #         namespace=kwargs.get('namespace'), vrf='default')
+        #     if not rdf.empty:
+        #         rdf['prefixlen'] = rdf['prefix'].str.split('/') \
+        #                                             .str[1].astype('int')
+        #     her_df["assertReason"] += her_df.apply(
+        #         self._is_vtep_reachable, args=(rdf,), axis=1)
 
         mcast_df = df.query('mcastGroup != "0.0.0.0"')
         if not mcast_df.empty:
@@ -262,18 +262,22 @@ class EvpnvniObj(SqPandasEngine):
                 and x['master'] != "bridge") else [],
             axis=1)
 
-        # mac_df = MacsObj(context=self.ctxt) \
-        #     .get(namespace=kwargs.get("namespace", ""),
-        #          macaddr=["00:00:00:00:00:00"])
+        mac_df = self._get_table_sqobj('macs') \
+            .get(namespace=kwargs.get("namespace", ""),
+                 macaddr=["00:00:00:00:00:00"], remoteVtepIp=['any'])
 
-        # # Assert that we have HER for every remote VTEP
-        # df['assertReason'] += df.apply(self._is_her_good,
-        #                                args=(mac_df, ), axis=1)
+        # # Assert that we have HER for every remote VTEP; Cumulus/SONiC
+        if not mac_df.empty:
+            df['assertReason'] += df.apply(self._is_her_good,
+                                           args=(mac_df, ), axis=1)
 
         # Fill out the assert column
         df['result'] = df.apply(lambda x: 'pass'
                                 if len(x.assertReason) == 0 else 'fail',
                                 axis=1)
+
+        # Force VNI to be an int
+        df['vni'] = df.vni.astype(int)
 
         if result == 'fail':
             df = df.query('assertReason.str.len() != 0')
