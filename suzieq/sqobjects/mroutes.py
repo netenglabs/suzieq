7a543eefdbd97660a439cee10b7b3e649ccc8f4d
scetron <stephen.corry@networktocode.com>
2022-04-26 21:23:44 +0000

    add initial mroutes to sqobects

---
+++ suzieq/sqobjects/mroutes.py
@@ -0,0 +1,38 @@
+from suzieq.sqobjects.basicobj import SqObject
+
+
+class MroutesObj(SqObject):
+    '''The object providing access to the mroutes table
+    keys:
+        - source
+        - group
+        - vrf
+    ignore-fields:
+        - statusChangeTimestamp
+    show-fields:
+        - source
+        - group
+        - rpfInterface
+        - oifList
+        - vrf
+        - ipvers
+        - rp
+        - rpfneighbor
+
+
+    keys:
+  - vlanName
+
+show-fields:
+  - vlanName
+  - vlan
+  - interfaces
+    
+    '''
+
+    def __init__(self, **kwargs):
+        super().__init__(table='mroutes', **kwargs)
+        self._valid_get_args = ['namespace', 'hostname', 'columns', 'source', 'group', 'vrf', 'rpfInterface', 
+                                'oifList', 'ipvers', 'rp', 'rpfneighbor']
+        # self._valid_arg_vals = {'state': ['active', 'suspended', '']}
+        # self._unique_def_column = ['source']
