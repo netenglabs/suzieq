4364d35b8c781c5fb523dc18eb5a0884b73f76ed
scetron <stephen.corry@networktocode.com>
2022-05-10 20:51:17 +0000

    fadjust pandas/mroutes

--- suzieq/engines/pandas/mroutes.py
+++ suzieq/engines/pandas/mroutes.py
@@ -24,21 +24,13 @@ class MroutesObj(SqPandasEngine):
         vrf = kwargs.pop('vrf', '')
         ipvers = kwargs.pop('ipvers', '')
         user_query = kwargs.pop('query_str', '')
-
         columns = kwargs.pop('columns', ['default'])
         fields = self.schema.get_display_fields(columns)
 
-        print(fields)
-
         addnl_fields = ['source', 'group', 'rpfInterface', 'oifList', 'rpNeighbor']
-
-        # /32 routes are stored with the /32 prefix, so if user doesn't specify
-        # prefix as some folks do, assume /32
-
 
         df = super().get(addnl_fields=addnl_fields, source=source,
                          ipvers=ipvers, columns=fields, **kwargs)
-
 
         if user_query:
             df = self._handle_user_query_str(df, user_query)
