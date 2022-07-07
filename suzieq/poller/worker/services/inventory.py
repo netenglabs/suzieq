de18f216a7db57e4957acacf279698c4fcafc792
jefvantongerloo <jefvantongerloo@gmail.com>
2022-07-07 20:00:17 +0200

    Regex as raw string
    Signed-off-by: jefvantongerloo <jefvantongerloo@gmail.com>

--- suzieq/poller/worker/services/inventory.py
+++ suzieq/poller/worker/services/inventory.py
@@ -34,7 +34,7 @@ class InventoryService(Service):
             type = entry['description']
             partType = entry['model_type']
             
-            version_result = re.search(r'(\d+\.){3,}.*?(?=,)', system.get('description'))
+            version_result = re.search('(\d+\.){3,}.*?(?=,)', system.get('description'))
             version = version_result.group(0)
 
             if entry.get('admin_status') == 'POWER ON' and entry.get('operational_status') == 'UP':
