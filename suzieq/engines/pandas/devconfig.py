51e477939fbe48442b3c563f7c79d6e8d13fe5c0
Dinesh Dutt <dd.ps4u@gmail.com>
2024-05-26 22:16:02 -0700

    engines/devconfig: add support for searching a line/section
    Signed-off-by: Dinesh Dutt <dd.ps4u@gmail.com>

--- suzieq/engines/pandas/devconfig.py
+++ suzieq/engines/pandas/devconfig.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from ciscoconfparse import CiscoConfParse
+
 from suzieq.engines.pandas.engineobj import SqPandasEngine
 
 
@@ -8,3 +12,47 @@ class DevconfigObj(SqPandasEngine):
     def table_name():
         '''Table name'''
         return 'devconfig'
+
+    def get(self, **kwargs):
+        '''Retrieve the devconfig table info'''
+
+        section = kwargs.pop('section', None)
+        columns = kwargs.pop('columns', ['default'])
+        query_str = kwargs.pop('query_str', '')
+
+        df = super().get(columns=columns, **kwargs)
+        if df.empty or 'error' in df.columns or not section:
+            return df
+
+        devdf = self._get_table_sqobj('device') \
+            .get(columns=['namespace', 'hostname', 'os'], **kwargs)
+
+        if devdf.empty or 'errror' in devdf.columns:
+            return df
+
+        drop_indices: List[int] = []
+        for index, row in enumerate(df.itertuples()):
+            os = devdf.loc[(devdf['hostname'] == row.hostname) &
+                           (devdf['namespace'] == row.namespace),
+                           'os'].values[0]
+            if os.startswith('junos') or os == 'panos':
+                os = 'junos'
+            elif os == 'nxos':
+                os = 'nxos'
+            else:
+                os = 'ios'
+
+            parsed_conf = CiscoConfParse(row.config.split('\n'), syntax=os)
+            conf = '\n'.join(parsed_conf.find_all_children(section))
+            if not conf:
+                drop_indices.append(index)
+            else:
+                df.loc[index, 'config'] = conf
+
+        if drop_indices:
+            df = df.drop(index=drop_indices)
+
+        if query_str:
+            df = df.query(query_str).reset_index(drop=True)
+
+        return df
