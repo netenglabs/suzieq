89e4cb6244fa4a174d7b81d893bd5391d22dc9f3
Sepehr-A <sepehr@stardustsystems.net>
2026-07-10 15:05:20 +0200

    panda/engineobj: fix var type
    Signed-off-by: Sepehr-A <sepehr@stardustsystems.net>

--- suzieq/engines/pandas/engineobj.py
+++ suzieq/engines/pandas/engineobj.py
@@ -455,7 +455,7 @@ class SqPandasEngine(SqEngineObj):
         is upto the caller to ensure that the desired column is in the output.
         """
         what = kwargs.pop("what", None)
-        reverse = kwargs.pop("reverse", False)
+        reverse = bool(str(kwargs.pop("reverse", "False")).lower() == "true")
         sqTopCount = kwargs.pop("count", 5)
         columns = kwargs.pop("columns", ['default'])
 
